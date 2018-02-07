13ae7eb917daa494aa0d95a1edd3f936e9a5c461
Masaya Kudo <msykd16@gmail.com>
2018-02-07 17:25:04 +0900

    メッセージ一覧実装

--- src/containers/Messages.js
+++ src/containers/Messages.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { withStyles } from 'material-ui/styles';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
 import { defaultPageFactory } from '../components/PageLayouts';
 
 class Messages extends React.Component {
@@ -8,13 +10,79 @@ class Messages extends React.Component {
     super(props);
     this.pageTitle = 'メッセージ';
     this.contents = this.contents.bind(this);
+    this.messages = [
+      {
+
+      }
+    ];
   }
 
   contents() {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        test
+        <List component="nav">
+          <ListItem button divider>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+          <ListItem button divider>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+          <ListItem button divider>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+          <ListItem button divider>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+          <ListItem button divider>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+          <ListItem button divider>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+          <ListItem button divider>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+          <ListItem button divider>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+          <ListItem button divider>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+          <ListItem button>
+            <Avatar>
+              MK
+            </Avatar>
+            <ListItemText primary="Masaya Kudo" secondary="こんにちは。来月から荷物を預かっていただきたいのですがお願い..." />
+          </ListItem>
+        </List>
       </div>
     );
   }
@@ -26,8 +94,12 @@ class Messages extends React.Component {
 
 }
 
-const styles = {
-
-};
+const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 720,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
 
 export default withStyles(styles)(Messages);
