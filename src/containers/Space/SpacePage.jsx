3e8afcd3c295d4aa330cb93aec7a42a64094a088
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-02-18 02:25:34 +0900

    add space page

--- src/containers/Space/SpacePage.jsx
+++ src/containers/Space/SpacePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import RequestDialog from './RequestDialog';
 import SpaceMenu from '../Search/SpaceMenu';
@@ -16,8 +17,15 @@ import {
   HostInfo,
 } from '../../stories/space';
 
-export default () => (
-  <Card>
+const SpacePage = styled.div`
+`;
+
+const SpaceCardContainer = styled.div`
+  width: 688px;
+`;
+
+export default props => (
+  <SpacePage>
     {/* TODO マップのprops調整 */}
     <Map
       containerElement={<div style={{ height: '300px' }} />}
@@ -25,5 +33,28 @@ export default () => (
       loadingElement={<div style={{ height: '100%' }} />}
       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrHQDZXZI21cMEW8FIYYWKyvI2kLUDsbA&v=3.exp&libraries=geometry,drawing,places"
     />
-  </Card>
+    <SpaceCardContainer>
+      <Card>
+        <PlaceText>東京都 港区 六本木</PlaceText>
+        <HeaderTitle>東京タワーに近くて便利！大きい荷物も何人ぶんでもOK</HeaderTitle>
+        <SlideImage
+          images={[
+            {
+              original: 'http://placehold.jp/500x300.png',
+              thumbnail: 'http://placehold.jp/500x300.png',
+            },
+            {
+              original: 'http://placehold.jp/500x300.png',
+              thumbnail: 'http://placehold.jp/500x300.png',
+            },
+            {
+              original: 'http://placehold.jp/500x300.png',
+              thumbnail: 'http://placehold.jp/500x300.png',
+            },
+          ]}
+        />
+        <Caption>閲覧頂き有り難うございます！赤羽橋、芝公園などの駅付近で預かることが可能です。玄関から入れば大きめの荷物も対応可能です！</Caption>
+      </Card>
+    </SpaceCardContainer>
+  </SpacePage>
 );
