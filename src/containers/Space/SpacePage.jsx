0faaa1360c05f6850751f29665d28b4b0e764430
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-02-18 03:34:48 +0900

    mod map height

--- src/containers/Space/SpacePage.jsx
+++ src/containers/Space/SpacePage.jsx
@@ -47,7 +47,7 @@ export default props => (
   <SpacePage>
     {/* TODO マップのprops調整 */}
     <Map
-      containerElement={<div style={{ marginTop: '-20px', height: '300px' }} />}
+      containerElement={<div style={{ marginTop: '-20px', height: '360px' }} />}
       mapElement={<div style={{ height: '100%' }} />}
       loadingElement={<div style={{ height: '100%' }} />}
       googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCrHQDZXZI21cMEW8FIYYWKyvI2kLUDsbA&v=3.exp&libraries=geometry,drawing,places"
