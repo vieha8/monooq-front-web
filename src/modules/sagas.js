c8bbc1806b8ffc4efb9078db07d8913aef6e0fed
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-02-22 01:10:33 +0900

    Revert "add test try-catch"
    This reverts commit 8c7fa5e52529c1557a5a65b6e7ff75d4bdef0364.

--- src/modules/sagas.js
+++ src/modules/sagas.js
@@ -4,10 +4,5 @@ import { searchSagas } from './search';
 import { messagesSagas } from './messages';
 
 export default function* rootSaga() {
-  try {
-    yield all([...authSagas, ...searchSagas, ...messagesSagas]);
-  } catch (e) {
-    // TODO error handing
-    console.log(e);
-  }
+  yield all([...authSagas, ...searchSagas, ...messagesSagas]);
 }
