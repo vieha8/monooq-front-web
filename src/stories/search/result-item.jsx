d00f816f285a7171a4f71161966c0f56f37eef47
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-02-22 03:35:14 +0900

    fix adjust semantic-ui search

--- src/stories/search/result-item.jsx
+++ src/stories/search/result-item.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Card } from 'semantic-ui-react';
 import { media } from '../../helpers/style/media-query';
 import { Colors, FontSizes, Dimens } from '../../variables';
 
 const Container = styled.div`
-  border-radius: 3px;
-  border: 1px solid ${Colors.borderGray};
   width: 237px;
-  height: 331px;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.4);
   cursor: pointer;
   margin: ${Dimens.medium3}px ${Dimens.medium}px 0 ${Dimens.medium}px;
-
   ${media.phone`
     width: 160px;
     margin: ${Dimens.xsmall}px;
   `}
+
+  box-shadow: none;
+  transition: 0.3s;
+  &:hover {
+    box-shadow: 0 2px 4px rgba(0,0,0,0.4);
+    transition: 0.3s; 
+  }
 `;
 
 const Content = styled.div`
@@ -56,16 +59,18 @@ export default props => (
   <Container
     onClick={() => window.open('/space/1')}
   >
-    <Image
-      src="http://placehold.jp/200x150.png"
-      alt=""
-    />
-    <Content>
-      <PlaceText>{props.place}</PlaceText>
-      <Text>{props.name}</Text>
-      {props.typeOK && <TypeOK>家具・家電OK</TypeOK>}
-      <PriceTitle>料金目安（30日間）</PriceTitle>
-      <Price>{props.price}</Price>
-    </Content>
+    <Card>
+      <Image
+        src="http://placehold.jp/200x150.png"
+        alt=""
+      />
+      <Content>
+        <PlaceText>{props.place}</PlaceText>
+        <Text>{props.name}</Text>
+        {props.typeOK && <TypeOK>家具・家電OK</TypeOK>}
+        <PriceTitle>料金目安（30日間）</PriceTitle>
+        <Price>{props.price}</Price>
+      </Content>
+    </Card>
   </Container>
 );
