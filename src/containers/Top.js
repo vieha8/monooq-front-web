2882911e9dc873d01060b30d9f6046f76f200fe0
ksyunnnn <l.o.s19kk@gmail.com>
2018-02-21 20:57:14 +0900

    PC表示のみ

--- src/containers/Top.js
+++ src/containers/Top.js
@@ -7,7 +7,9 @@ import Hidden from 'material-ui/Hidden';
 import SearchIcon from 'material-ui-icons/Search';
 
 import styled from 'styled-components';
-import { Colors } from '../variables';
+import { FontSizes, Colors } from '../variables';
+import { media, isMobileWindow } from '../helpers/style/media-query';
+import { Footer } from '../stories/shared';
 
 const IMAGE_URL = 'https://picsum.photos/1280/800?image=1012';
 
@@ -44,7 +46,7 @@ const CatchPhrase = styled.div`
 `;
 
 const SubCatchPhrase = styled.span`
-  display: inline-block;
+  display: block;
   font-family: sans-serif;
   font-size: 26px;
   width: 561px;
@@ -81,18 +83,44 @@ const TopHr = styled.hr`
   margin: 0;
   height: 60px;
   border: 0;
-  background: rgb(232, 82, 88);
+  background: #e85258;
 `;
 
 const DefaultView = styled.div`
   padding: 80px 116px;
-  background-size: cover;
   color: rgb(51, 51, 51);
   box-sizing: border-box;
   ${props =>
-    props.isReason
+    props.isReasonUser
       ? `
         background: #fff;
+        background-image: url('http://placehold.jp/714x535.png');
+        background-repeat: no-repeat;
+        background-position: right;
+        background-position-y: 388px;
+    `
+      : ''};
+  ${props =>
+    props.isReasonHost
+      ? `
+        background-image: url('http://placehold.jp/F7F7F7/eee/530x989.png');
+        background-repeat: no-repeat;
+        background-position: left;
+        padding: 80px 116px 80px 656px;
+    `
+      : ''};
+  ${props =>
+    props.isForSafe
+      ? `
+        background: #fff;
+    `
+      : ''};
+  ${props =>
+    props.isPickGo
+      ? `
+        background: #E85258;
+        color: #fff;
+        display: flex;
     `
       : ''};
 `;
@@ -128,7 +156,7 @@ const ExplainDescription = styled.span`
   font-weight: 100;
 `;
 
-const ExplainSection = ({ title, description, isRight = false }) => {
+const ExplainSection = ({ title, description }) => {
   const style = {
     root: {
       width: '417px',
@@ -138,6 +166,7 @@ const ExplainSection = ({ title, description, isRight = false }) => {
       marginBottom: '10px',
     },
   };
+
   return (
     <div className="explain-section" style={style.root}>
       <ExplainTitle style={style.title}>{title}</ExplainTitle>
@@ -145,6 +174,149 @@ const ExplainSection = ({ title, description, isRight = false }) => {
     </div>
   );
 };
+
+const ToHostRegistButton = styled.button`
+  display: inline-block;
+  background: ${Colors.buttonPink};
+  font-size: ${FontSizes.medium}px;
+  color: ${Colors.white};
+  border-radius: 3px;
+  width: 264px;
+  height: 50px;
+  box-sizing: border-box;
+  &:hover {
+    background: ${Colors.buttonPinkHover};
+  }
+  cursor: pointer;
+  border: none;
+  outline: none;
+
+  ${media.phone`
+    display: table-cell;
+    padding: 9px 20px;
+    vertical-align: middle;
+  `};
+`;
+
+const ForSafeSectionButton = styled.button`
+  display: inline-block;
+  color: ${Colors.buttonPink};
+  font-size: ${FontSizes.medium}px;
+  background: ${Colors.white};
+  border-radius: 3px;
+  width: 100%;
+  height: 50px;
+  box-sizing: border-box;
+  &:hover {
+    background: ${Colors.buttonPinkHover};
+    color: ${Colors.white};
+  }
+  cursor: pointer;
+  border: 1px solid;
+  outline: none;
+
+  ${media.phone`
+    display: table-cell;
+    padding: 9px 20px;
+    vertical-align: middle;
+  `};
+`;
+
+const ForSafeSection = ({ iconClass, title, description, buttonText }) => {
+  const style = {
+    root: {
+      width: '327px',
+      fontFamily: 'sans-serif',
+      fontWeight: 100,
+    },
+    title: {
+      height: '60px',
+      lineHeight: '60px',
+      fontSize: '22px',
+      marginBottom: '20px',
+    },
+    titleIcon: {
+      marginRight: '20px',
+      fontSize: '60px',
+    },
+    description: {
+      fontSize: '16px',
+      lineHeight: '32px',
+      marginBottom: '30px',
+    },
+  };
+  return (
+    <div style={style.root}>
+      <div style={style.title}>
+        <span className="fa-layers fa-fw fa-2x" style={style.titleIcon}>
+          <i className="fas fa-circle" style={{ color: '#E85258' }} />
+          <i className={iconClass} style={{ color: '#fff' }} data-fa-transform="shrink-6" />
+        </span>
+        <span>{title}</span>
+      </div>
+      <div style={style.description}>{description}</div>
+      <ForSafeSectionButton>{buttonText}</ForSafeSectionButton>
+    </div>
+  );
+};
+
+const ForSafeSectionList = () => {
+  const style = {
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-between',
+    },
+  };
+  return (
+    <div className="for-safe-section-list" style={style.root}>
+      <ForSafeSection
+        iconClass="far fa-heart"
+        title="はじめての方へ"
+        description="モノオクは物置きの場所を探して、ホストにメッセージで荷物を預かってもらえるか交渉をします。サービスの使い方がよくわからない、お困りの方はこちら。"
+        buttonText="使い方について"
+      />
+      <ForSafeSection
+        iconClass="far fa-bookmark"
+        title="荷物に対する保険"
+        description="モノオクで取引された荷物には最大10万円までの補償があります。もし預かった荷物を破損・紛失してしまった場合も最大100000円までの補償が受けられるので安心です。"
+        buttonText="保険について"
+      />
+      <ForSafeSection
+        iconClass="far fa-handshake"
+        title="ルールとマナー"
+        description="モノオクは個人間の物置きシェアサービスです。トラブルや揉め事がないようにルールを設けています。みんなが安心して使えるようにマナーを大切にしましょう。"
+        buttonText="ルールとマナーについて"
+      />
+    </div>
+  );
+};
+
+const LinkToPickGo = styled.a`
+  font-family: sans-serif;
+  font-size: 18px;
+  color: rgb(255, 255, 255);
+  line-height: 42px;
+`;
+
+const PickGoSection = styled.div`
+  width: 294px;
+  color: #333333;
+  text-align: center;
+  margin-left: 181px;
+  margin-top: 87px;
+`;
+
+const PickGoMedia = styled.img`
+  margin: 0 15.5px;
+  margin-bottom: 20px;
+`;
+
+const PickGoDescription = styled.div`
+  font-family: sans-serif;
+  font-size: 14px;
+  line-height: 27px;
+`;
 
 class Top extends React.Component {
   constructor(props) {
@@ -210,7 +382,7 @@ class Top extends React.Component {
           </div>
         </TopView>
         <TopHr />
-        <DefaultView isReason>
+        <DefaultView isReasonUser>
           <HilightCopy>小さいモノから大きなモノまで。</HilightCopy>
           <DefaultTitle>モノオクで荷物を預ける理由</DefaultTitle>
           <ExplainSection
@@ -234,7 +406,7 @@ class Top extends React.Component {
             description="もう少し延長できませんか？荷物の搬入を手伝ってくれませんか？など、ホストと気軽に交渉することが可能です。"
           />
         </DefaultView>
-        <DefaultView>
+        <DefaultView isReasonHost>
           <HilightCopy>余っているスペースはありませんか？</HilightCopy>
           <DefaultTitle>モノオクでホストをする理由</DefaultTitle>
           <ExplainSection
@@ -253,7 +425,33 @@ class Top extends React.Component {
             title="誰かの役にたつ"
             description="引っ越しやリフォーム、片付けなど荷物を預けられる場所を探すのは意外と大変なんです。困っている誰かの力になってくれませんか？"
           />
+          <ToHostRegistButton>ホスト登録はこちら</ToHostRegistButton>
         </DefaultView>
+        <DefaultView isForSafe>
+          <HilightCopy>みんなでもっと便利に物置きシェアができる世の中へ。</HilightCopy>
+          <DefaultTitle>安心して物置きのシェアをするために</DefaultTitle>
+          <ForSafeSectionList />
+        </DefaultView>
+        <DefaultView isPickGo>
+          <div>
+            <CatchPhrase>
+              荷物の配送だって<br />もっと便利に安くできる。
+            </CatchPhrase>
+            <SubCatchPhrase>
+              Pickgoを使えば、引っ越しが5000円から。<br />モノオクから簡単に配送依頼ができます。
+            </SubCatchPhrase>
+            <LinkToPickGo component={Link} href={'#'}>
+              Pickgoのサイトを見る
+            </LinkToPickGo>
+          </div>
+          <PickGoSection>
+            <PickGoMedia src="http://placehold.jp/163x42.png" />
+            <PickGoDescription>
+              サービス対象エリア<br />東京／神奈川／千葉／埼玉／大阪／兵庫／京都
+            </PickGoDescription>
+          </PickGoSection>
+        </DefaultView>
+        <Footer />
       </TopPage>
     );
   }
