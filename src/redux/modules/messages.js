9b075166febb08877cab865ddb0b9dd8505cdbbd
Masaya Kudo <msykd16@gmail.com>
2018-03-07 14:32:57 +0900

    メッセージのReducerちょいリファクタリング

--- src/redux/modules/messages.js
+++ src/redux/modules/messages.js
@@ -28,78 +28,34 @@ const initialState = {
   isLoading: false,
 };
 
-const {
-  fetchRoomsStart,
-  fetchRoomsEnd,
-  fetchMessagesStart,
-  fetchMessagesEnd,
-  sendMessage,
-} = messagesActions;
 export const messagesReducer = handleActions(
   {
-    [fetchRoomsStart]: state => ({ ...state, isLoading: true }),
-    [fetchRoomsEnd]: (state, action) => ({ ...state, isLoading: false, rooms: action.payload }),
-    [fetchMessagesStart]: state => ({ ...state, isLoading: true }),
-    [fetchMessagesEnd]: (state, action) => ({
+    [FETCH_ROOMS_START]: state => ({ ...state, isLoading: true }),
+    [FETCH_ROOMS_END]: (state, action) => ({ ...state, isLoading: false, rooms: action.payload }),
+    [FETCH_MESSAGES_START]: state => ({ ...state, isLoading: true }),
+    [FETCH_MESSAGES_END]: (state, action) => ({
       ...state,
       isLoading: false,
       messages: action.payload,
     }),
-    [sendMessage]: state => ({ ...state }),
+    [SEND_MESSAGE]: state => ({ ...state }),
   },
   initialState,
 );
 
 //Sagas
 export const messagesSagas = [
-  takeEvery(FETCH_ROOMS_START, function*(action) {
-    const rooms = yield call(() => getRooms(action.payload));
-    yield put(fetchRoomsEnd(rooms));
+  takeEvery(FETCH_ROOMS_START, function*({ payload }) {
+    const rooms = yield getRooms(payload);
+    yield put(messagesActions.fetchRoomsEnd(rooms));
   }),
-  takeEvery(FETCH_MESSAGES_START, function*(action) {
-    const messages = yield call(() => getMessages(action.payload));
-    yield put(fetchMessagesEnd(messages));
+  takeEvery(FETCH_MESSAGES_START, function*({ payload }) {
+    const messages = yield getMessages(payload);
+    yield put(messagesActions.fetchMessagesEnd(messages));
   }),
-  takeEvery(SEND_MESSAGE, function*(action) {
-    const { roomId, userId, text, image } = action.payload;
-
-    let imageUrl;
-    if (image) {
-      const fileReader = new FileReader();
-      fileReader.readAsArrayBuffer(image);
-      const ext = yield call(() => {
-        return new Promise(resolve => {
-          fileReader.onload = () => {
-            const imageType = fileType(fileReader.result);
-            resolve(imageType.ext);
-          };
-        });
-      });
-      const timeStamp = Date.now();
-      imageUrl = yield call(() => uploadImage(`/${roomId}/${userId}/${timeStamp}.${ext}`, image));
-    }
-
-    yield call(() => {
-      return new Promise(async resolve => {
-        const db = firebase.firestore();
-        const message = {
-          userId: userId,
-          text: text,
-          messageType: 1,
-          createDt: new Date(),
-        };
-        if (imageUrl) {
-          message.image = imageUrl;
-        }
-        await db
-          .collection('rooms')
-          .doc(roomId)
-          .collection('messages')
-          .add(message);
-        resolve();
-      });
-    });
-    yield put(fetchMessagesStart(roomId));
+  takeEvery(SEND_MESSAGE, function*({ payload }) {
+    yield sendMessage(payload);
+    yield put(messagesActions.fetchMessagesStart(payload.roomId));
   }),
 ];
 
@@ -170,5 +126,45 @@ const getMessages = roomId => {
       });
     });
     resolve(res);
+  });
+};
+
+//メッセージ送信
+const sendMessage = function*(payload) {
+  const { roomId, userId, text, image } = payload;
+
+  let imageUrl;
+  if (image) {
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(image);
+    const ext = yield call(() => {
+      return new Promise(resolve => {
+        fileReader.onload = () => {
+          const imageType = fileType(fileReader.result);
+          resolve(imageType.ext);
+        };
+      });
+    });
+    const timeStamp = Date.now();
+    imageUrl = yield call(() => uploadImage(`/${roomId}/${userId}/${timeStamp}.${ext}`, image));
+  }
+
+  return yield new Promise(async resolve => {
+    const db = firebase.firestore();
+    const message = {
+      userId: userId,
+      text: text,
+      messageType: 1,
+      createDt: new Date(),
+    };
+    if (imageUrl) {
+      message.image = imageUrl;
+    }
+    await db
+      .collection('rooms')
+      .doc(roomId)
+      .collection('messages')
+      .add(message);
+    resolve();
   });
 };
