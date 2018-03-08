612b382de51eba8f7e336322c5aac972202960b3
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-03-09 01:54:05 +0900

    add logout action

--- src/containers/Logout.jsx
+++ src/containers/Logout.jsx
@@ -3,8 +3,14 @@ import { connect } from 'react-redux';
 import Page from 'components/Page';
 import Menu from 'containers/Menu';
 import Logout from 'components/Logout';
+import { authActions } from 'redux/modules/auth';
 
 class LogoutContainer extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(authActions.logout());
+  }
+
   render() {
     return (
       <Page title="ログアウト">
