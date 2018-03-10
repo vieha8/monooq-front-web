19d5c2af4fecf60ec51651013ea1b168c7a9424e
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-03-11 03:50:33 +0900

    add square image

---
+++ src/components/atoms/SauareImage/index.js
@@ -0,0 +1,10 @@
+// @flow
+
+import styled from 'styled-components';
+
+const SquareImage = styled.img`
+  width: ${props => props.size}px;
+  height: ${props => props.size}px;
+`;
+
+export default SquareImage;
