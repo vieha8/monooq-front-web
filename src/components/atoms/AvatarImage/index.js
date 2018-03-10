7075e2f7cdad731be4686c340d6620e0571abb7f
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-03-11 02:50:09 +0900

    add avatar image

---
+++ src/components/atoms/AvatarImage/index.js
@@ -0,0 +1,12 @@
+// @flow
+
+import styled from 'styled-components';
+
+const DEFAULT_SIZE = 64;
+const AvaterImage = styled.img`
+  width: ${props => props.size || DEFAULT_SIZE}px;
+  height: ${props => props.size || DEFAULT_SIZE}px;
+  border-radius: ${props => (props.size || DEFAULT_SIZE) / 2}px;
+`;
+
+export default AvaterImage;
