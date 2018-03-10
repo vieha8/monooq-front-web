fed0d64ed04e5b80e136d5e17885ff2b6f505ff3
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-03-11 00:38:00 +0900

    dev

--- src/components/atoms/TextLink/index.js
+++ src/components/atoms/TextLink/index.js
@@ -7,16 +7,31 @@ const TextLink = styled.a`
   color: ${Colors.linkBlue};
   line-height: 1.6;
   font-size: 16px;
-  text-decoration: none;
+  cursor: pointer;
 
+  &:link {
+    color: ${Colors.linkBlue};
+  }
+  &:visited {
+    color: ${Colors.linkBlue};
+  }
   &:hover {
+    color: ${Colors.linkBlue};
     opacity: 0.8;
+  }
+  &:active {
+    color: ${Colors.linkBlue};
   }
 
   ${props =>
     props.disabled &&
     `
+    cursor: not-allowed;
     color: ${Colors.lightGray1};
+    &:link { color: ${Colors.lightGray1}; }
+    &:visited { color: ${Colors.lightGray1}; }
+    &:hover { color: ${Colors.lightGray1}; opacity: 1; }
+    &:active { color: ${Colors.lightGray1}; }
   `} ${props =>
       props.small &&
       `
