0e3b661e164b77339c8e91ededcb3e2ce9b86f7d
Masaya Kudo <msykd16@gmail.com>
2018-03-10 16:25:58 +0900

    update api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export default () => {
   const configDevelopment = {
-    baseURI: 'https://monooq-api-dev.ap-northeast-1.elasticbeanstalk.com/v1',
+    baseURI: 'https://dev-api.monooq.com/v1',
   };
   // TODO return production conf
   const configProduction = configDevelopment;
