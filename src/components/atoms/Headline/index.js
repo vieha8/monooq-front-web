93ee9d5062699651899b7163dfaf5c680b3a1e59
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-03-12 02:02:07 +0900

    add headline

---
+++ src/components/atoms/Headline/index.js
@@ -0,0 +1,29 @@
+// @flow
+
+import styled from 'styled-components';
+import { Colors } from 'variables';
+import { media } from 'helpers/style/media-query';
+
+export const H1 = styled.h1`
+  color: ${Colors.black};
+  font-size: 34px;
+  line-height: 1.6;
+  font-weight: bold;
+  ${media.phone`
+    font-size: 22px;
+  `};
+`;
+
+export const H2 = H1.withComponent('h2').extend`
+  font-size: 30px;
+  ${media.phone`
+    font-size: 20px;
+  `}
+`;
+
+export const H3 = H1.withComponent('h3').extend`
+  font-size: 16px;
+  ${media.phone`
+    font-size: 14px;
+  `}
+`;
