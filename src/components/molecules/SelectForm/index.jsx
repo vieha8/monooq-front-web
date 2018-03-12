8a068f107eb617a6b20d9b43f4d43739047251f3
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-03-13 00:42:51 +0900

    add select form

---
+++ src/components/molecules/SelectForm/index.jsx
@@ -0,0 +1,38 @@
+// @flow
+
+import React, { Fragment } from 'react';
+import styled from 'styled-components';
+import { H3 } from 'components/atoms/Headline';
+import Select from 'components/atoms/Select';
+
+const SelectWrapper = styled.div`
+  margin-top: 4px;
+`;
+
+type PropTypes = {
+  label: string,
+  options: Array<{
+    name: string,
+    value: any,
+  }>,
+  hint?: string,
+  value: string,
+  onChange: Function,
+}
+
+export default (props: PropTypes) => (
+  <Fragment>
+    <H3>{props.label}</H3>
+    {props.hint && <InlineText.Emphasis>{props.hint}</InlineText.Emphasis>}
+    <SelectWrapper>
+      <Select
+        value={props.value}
+        onChange={props.onChange}
+      >
+        {props.options.map((option, i) => (
+          <option key={`select_form_option_${i}`} value={option.value}>{option.text}</option>
+        ))}
+      </Select>
+    </SelectWrapper>
+  </Fragment>
+);
---
+++ src/components/molecules/SelectForm/mock/SelectForm.jsx
@@ -0,0 +1,23 @@
+// @flow
+
+import React from 'react';
+
+import SelectForm from '../index';
+
+export default class HeaderMock extends React.Component {
+  constructor(props: Object) {
+    super(props);
+    this.state = { value: '' };
+  }
+
+  render() {
+    const { value } = this.state;
+    return (
+      <SelectForm
+        {...this.props}
+        value={value}
+        onChange={e => this.setState({ value: e.target.value })}
+      />
+    );
+  }
+}
