5aabc74626c3795febd9423065f5212148c66430
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-03-14 01:29:11 +0900

    add host entry

---
+++ src/components/atoms/HostEntry/stories.jsx
@@ -0,0 +1,13 @@
+// @flow
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import HostEntry from './index';
+
+storiesOf('Atoms/Util/HostEntry', module)
+  .add('Normal', () => (
+    <div>
+      <HostEntry onClick={() => console.log('onClick HostEntry')} />
+    </div>
+  ));
