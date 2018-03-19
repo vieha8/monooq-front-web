a23b48306aac7e333f6a5cfd7a5bbcd86313585e
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-03-20 00:41:31 +0900

    fix remove

--- src/containers/NewSpace/SpaceInfo.jsx
+++ src/containers/NewSpace/SpaceInfo.jsx
@@ -2,31 +2,40 @@ import React from 'react';
 import { authConnect } from "../../components/Auth";
 import { Page } from 'components/NewSpace/page/Shared';
 import SpaceInfo from 'components/NewSpace/page/SpaceInfo';
-import {uiActions} from "../../redux/modules/ui";
+import { uiActions } from "../../redux/modules/ui";
 
 class SpaceInfoContainer extends React.Component {
 
-  handleChangeText = ({target}) => {
-    const {space} = this.props.ui;
-    Object.assign(space, {[target.name]: target.value});
-    this.props.dispatch(uiActions.setUiState({space}));
+  handleChangeText = ({ target }) => {
+    const { space } = this.props.ui;
+    Object.assign(space, { [target.name]: target.value });
+    this.props.dispatch(uiActions.setUiState({ space }));
   };
 
   handleChangeSelect = (_, target) => {
-    this.handleChangeText({target});
+    this.handleChangeText({ target });
   };
 
   handleChangeImage = (accepted, rejected) => {
-    if(rejected.length > 0){
+    if (rejected.length > 0) {
       console.error(rejected);
     }
-    if(accepted.length > 4) {
+    if (accepted.length > 4) {
       return;
     }
-    const {space} = this.props.ui;
-    Object.assign(space, {images: [...space.images, ...accepted]});
-    this.props.dispatch(uiActions.setUiState({space}));
+    const { space } = this.props.ui;
+    Object.assign(space, { images: [...space.images, ...accepted] });
+    this.props.dispatch(uiActions.setUiState({ space }));
   };
+
+  onClickImageDelete = (deleteTargetIndex) => {
+    const { ui, dispatch } = this.props;
+    const { space } = ui;
+    const nextImages = Object.assign([], space.images);
+    nextImages.splice(deleteTargetIndex, 1);
+    Object.assign(space, { images: nextImages });
+    dispatch(uiActions.setUiState({ space }));
+  }
 
   render() {
     return (
@@ -36,6 +45,7 @@ class SpaceInfoContainer extends React.Component {
           handleChangeText={this.handleChangeText}
           handleChangeSelect={this.handleChangeSelect}
           handleChangeImage={this.handleChangeImage}
+          onClickImageDelete={this.onClickImageDelete}
         />
       </Page>
     );
