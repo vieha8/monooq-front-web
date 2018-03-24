bfc693d708f5479ce80b736ae34d030ff7dd3fef
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-03-24 21:58:21 +0900

    use menu react-router-dom Link

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -9,58 +9,64 @@ export default (props) => {
     <Container showMobile={showMobile}>
       <Menu>
         <MenuItem>
-          <MenuLink href={Path.messages(userId)}>
+          <MenuLink to={Path.messages(userId)}>
             <MenuText>メッセージ</MenuText><NoticeCount count={messageCount} />
           </MenuLink>
         </MenuItem>
         <MenuItem>
-          <MenuLink href={Path.schedule(userId)}>
+          <MenuLink to={Path.schedule(userId)}>
             <MenuText>スケジュール</MenuText><NoticeCount count={scheduleCount} />
           </MenuLink>
         </MenuItem>
         {hasSpace && (
           <Fragment>
             <MenuItem>
-              <MenuLink href={Path.spaces(userId)}>
+              <MenuLink to={Path.spaces(userId)}>
                 <MenuText>スペースの管理</MenuText><NoticeCount count={scheduleCount} />
               </MenuLink>
             </MenuItem>
             <MenuItem>
-              <MenuLink href={Path.createSpaceInfo()}>
+              <MenuLink to={Path.createSpaceInfo()}>
                 <MenuText>スペースを追加する</MenuText><NoticeCount count={scheduleCount} />
               </MenuLink>
             </MenuItem>
             {/*<MenuItem>*/}
-              {/*<MenuLink href={Path.requestTransfer()}>*/}
+              {/*<MenuLink to={Path.requestTransfer()}>*/}
                 {/*<MenuText>振込申請</MenuText><NoticeCount count={scheduleCount} />*/}
               {/*</MenuLink>*/}
             {/*</MenuItem>*/}
           </Fragment>
         )}
         {/*<MenuItem>*/}
-          {/*<MenuLink href={Path.paid(userId)}>*/}
+          {/*<MenuLink to={Path.paid(userId)}>*/}
             {/*<MenuText>支払い履歴</MenuText>*/}
           {/*</MenuLink>*/}
         {/*</MenuItem>*/}
         {!hasSpace &&
           <MenuItem>
-            <MenuLink href={Path.createSpaceInfo(userId)}>
+            <MenuLink to={Path.createSpaceInfo(userId)}>
               <MenuText>ホスト登録する</MenuText>
             </MenuLink>
           </MenuItem>
         }
         <MenuItem>
-          <MenuLink href={Path.editProfile(userId)}>
+          <MenuLink to={Path.editProfile(userId)}>
             <MenuText>プロフィールを編集する</MenuText>
           </MenuLink>
         </MenuItem>
         {/*<MenuItem>*/}
-          {/*<MenuLink href={Path.inquiry(userId)}>*/}
+          {/*<MenuLink to={Path.inquiry(userId)}>*/}
             {/*<MenuText>お問い合わせ</MenuText>*/}
           {/*</MenuLink>*/}
         {/*</MenuItem>*/}
         <MenuItem>
-          <MenuLink onClick={props.onClickLogout}>
+          <MenuLink
+            onClick={(e) => {
+              e.preventDefault();
+              props.onClickLogout();
+            }}
+            to=""
+          >
             <MenuText>ログアウト</MenuText>
           </MenuLink>
         </MenuItem>
