f6ffb9d48c4d2a439799bbf5bc6e473227538868
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-04-05 01:12:50 +0900

    add map

---
+++ src/components/atomic/atoms/SpaceMap/index.jsx
@@ -0,0 +1,43 @@
+// @flow
+
+import React from 'react';
+import styled from 'styled-components';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { media } from 'helpers/style/media-query';
+import mapMarkerIcon from 'images/space_map_marker@2x.svg';
+
+const MapContainer = styled.div`
+  height: 360px;
+  ${media.tablet`
+    height: 240px;  
+  `}
+`;
+
+type PropTypes = {
+  lat: number,
+  lng: number,
+}
+
+const Map = withScriptjs(withGoogleMap((props: PropTypes) => (
+  <GoogleMap
+    defaultZoom={14}
+    defaultCenter={{ lat: props.lat + 0.003, lng: props.lng }}
+  >
+    <Marker
+      position={{ lat: props.lat - 0.003, lng: props.lng }}
+      icon={{
+        url: mapMarkerIcon,
+      }}
+    />
+  </GoogleMap>
+)));
+
+const KEY = 'AIzaSyCrHQDZXZI21cMEW8FIYYWKyvI2kLUDsbA';
+Map.defaultProps = {
+  containerElement: (<MapContainer />),
+  mapElement: (<div style={{ height: '100%' }} />),
+  loadingElement: (<div style={{ height: '100%' }} />),
+  googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${KEY}&v=3.exp&libraries=geometry,drawing,places`,
+};
+
+export default Map;
