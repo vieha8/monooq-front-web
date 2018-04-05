38f5a6fea44b4ec37816c12cf4923c4e8b30e0b8
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-04-06 01:33:39 +0900

    add space page molecules

---
+++ src/components/atomic/molecules/Space/Receive.jsx
@@ -0,0 +1,70 @@
+// @flow
+
+import React from 'react';
+import styled from 'styled-components';
+import InlineText from 'components/atomic/atoms/InlineText';
+import { Dimens, FontSizes } from 'variables';
+import { media } from 'helpers/style/media-query';
+import Attribute from './Attribute';
+
+const Container = styled.div`
+  ${media.phone`
+    margin-top: ${Dimens.medium}px;
+  `}
+`;
+
+const IconWrapper = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+const Icon = styled.i`
+  font-size: ${FontSizes.medium2}px;
+`;
+
+const Column = styled.span`
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: ${Dimens.small}px;
+`;
+
+type PropTypes = {
+  delivery: boolean,
+  meeting: boolean,
+}
+
+export default (props: PropTypes) => (
+  <Attribute
+    title="受取り方法"
+    content={
+      <Container>
+        {props.delivery &&
+          <div>
+            <IconWrapper>
+              <Icon className="fal fa-truck" />
+            </IconWrapper>
+            <Column>
+              <InlineText.Base>配送</InlineText.Base>
+            </Column>
+            <Column>
+              <InlineText.Emphasis>Pickgo・ヤマト運輸など配送サービス</InlineText.Emphasis>
+            </Column>
+          </div>
+        }
+        {props.meeting &&
+          <div>
+            <IconWrapper>
+              <Icon className="fal fa-users" />
+            </IconWrapper>
+            <Column>
+              <InlineText.Base>対面</InlineText.Base>
+            </Column>
+            <Column>
+              <InlineText.Emphasis>直接本人から荷物を受け取ります</InlineText.Emphasis>
+            </Column>
+          </div>
+        }
+      </Container>
+    }
+  />
+);
---
+++ src/components/atomic/molecules/Space/stories.jsx
@@ -0,0 +1,93 @@
+// @flow
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import StoryRouter from 'storybook-router';
+
+import Header from './Header';
+import Attribute from './Attribute';
+import Address from './Address';
+import Type from './Type';
+import AboutBaggage from './AboutBaggage';
+import Receive from './Receive';
+import Supplement from './Supplement';
+import HostInfo from './HostInfo';
+
+storiesOf('Molecules/Space', module)
+  .addDecorator(StoryRouter())
+  .add('Header', () => (
+    <div>
+      <Header
+        pref="東京都"
+        city="杉並区"
+        town="高円寺"
+        name="テストテストテストテストテストテスト"
+      />
+    </div>
+  ))
+  .add('Attribute', () => (
+    <div>
+      <Attribute
+        title="タイトル"
+        content={
+          <div>
+            <div>内容1</div>
+            <div>内容2</div>
+          </div>
+        }
+      />
+      <Attribute
+        title="タイトル"
+        content={
+          <div>
+            <div>内容1</div>
+            <div>内容2</div>
+          </div>
+        }
+      />
+      <Attribute
+        title="タイトル"
+        content={
+          <div>
+            <div>内容1</div>
+            <div>内容2</div>
+          </div>
+        }
+      />
+    </div>
+  ))
+  .add('Address', () => (
+    <div>
+      <Address content="東京都杉並区高円寺" />
+    </div>
+  ))
+  .add('Type', () => (
+    <div>
+      <Type content="東京都杉並区高円寺" />
+    </div>
+  ))
+  .add('AboutBaggage', () => (
+    <div>
+      <AboutBaggage content="東京都杉並区高円寺" />
+    </div>
+  ))
+  .add('Receive', () => (
+    <div>
+      <Receive delivery meeting />
+    </div>
+  ))
+  .add('Supplement', () => (
+    <div>
+      <Supplement content="受け取りは日曜日にお願いします" />
+    </div>
+  ))
+  .add('HostInfo', () => (
+    <div>
+      <HostInfo
+        id={100}
+        name="ものおくほすと"
+        profile="よろしくお願いします！"
+        imageUrl="http://placehold.jp/500x500.png"
+      />
+    </div>
+  ));
