079c981620c8f61db482164143451b7e248ff358
imuuzak-hori <kazuumi.hori@spacemarket.co.jp>
2018-04-07 17:04:36 +0900

    add templates space

---
+++ src/components/atomic/templates/Space/index.jsx
@@ -0,0 +1,93 @@
+// @flow
+
+import React from 'react';
+import styled from 'styled-components';
+import ClearfixContainer from 'components/atomic/atoms/ClearfixContainer';
+import { media } from 'helpers/style/media-query';
+import { Dimens } from 'variables';
+import Page from '../Page';
+
+const Map = styled.div`
+  height: 360px;
+  ${media.tablet`
+    height: 240px;  
+  `}
+`;
+
+const Container = styled.div`
+  margin: ${Dimens.medium3}px 0;
+  ${media.tablet`
+    margin: 0;
+  `}
+`;
+
+const DetailContainer = styled.div`
+  max-width: 700px;
+  width: 60%;
+  padding-bottom: 80px;
+  float: left;
+
+  ${media.tablet`
+    float: none;
+    width: 100%;
+    max-width: 100%;
+    margin: 0;
+  `}
+`;
+
+const PriceContainer = styled.div`
+  max-width: 300px;
+  width: 40%;
+  margin-left: ${Dimens.medium2}px;
+  float: left;
+
+  ${media.tablet`
+    float: none;
+    margin: 0;
+    width: 100%;
+    max-width: 100%;
+    padding-bottom: 40px;
+  `}
+`;
+
+const MarginTop = styled.div`
+  margin-top: ${Dimens.medium}px;
+`;
+
+const Footer = styled.div`
+  ${media.tablet`
+    display: none;
+  `}
+`;
+
+type PropTypes = {
+  header: React.Element<*>,
+  map: React.Element<*>,
+  detail: React.Element<*>,
+  price: React.Element<*>,
+  message: React.Element<*>,
+  footer: React.Element<*>,
+};
+
+export default (props: PropTypes) => (
+  <Page>
+    {props.header}
+    <Map>{props.map}</Map>
+    <Container>
+      <ClearfixContainer>
+        <DetailContainer>
+          {props.detail}
+        </DetailContainer>
+        <PriceContainer>
+          {props.price}
+          <MarginTop>
+            {props.message}
+          </MarginTop>
+        </PriceContainer>
+      </ClearfixContainer>
+    </Container>
+    <Footer>
+      {props.footer}
+    </Footer>
+  </Page>
+);
---
+++ src/components/atomic/templates/Space/stories.jsx
@@ -0,0 +1,78 @@
+// @flow
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import SpaceTemplate from './index';
+
+storiesOf('Templates/Space', module)
+  .add('Normal', () => (
+    <SpaceTemplate
+      header={(
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background: 'red',
+          }}
+        >
+          header section
+        </div>
+      )}
+      map={
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background: 'yellow',
+          }}
+        >
+          map section
+        </div>
+      }
+      detail={
+        <div
+          style={{
+            width: '100%',
+            height: '500px',
+            background: 'green',
+          }}
+        >
+          detail section
+        </div>
+      }
+      price={
+        <div
+          style={{
+            width: '100%',
+            height: '300px',
+            background: 'gray',
+          }}
+        >
+          price section
+        </div>
+      }
+      message={
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background: 'orange',
+          }}
+        >
+          message section
+        </div>
+      }
+      footer={(
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            background: 'blue',
+          }}
+        >
+          footer section
+        </div>
+      )}
+    />
+  ));
