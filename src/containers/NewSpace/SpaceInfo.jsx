83cbcfb7df44fd219a6fd0117d0fc301d70d4935
Masaya Kudo <msykd16@gmail.com>
2018-04-10 16:33:40 +0900

    スペース所在地入力のバリデーション追加

--- src/containers/NewSpace/SpaceInfo.jsx
+++ src/containers/NewSpace/SpaceInfo.jsx
@@ -72,6 +72,17 @@ class SpaceInfoContainer extends React.Component {
     if (value.length === 0) {
       errors.push(ErrorMessage.PleaseInput);
     }
+
+    let reg = `(...??[都道府県])`;
+    reg += `((?:旭川|伊達|石狩|盛岡|奥州|田村|南相馬|那須塩原|東村山|武蔵村山|羽村|十日町|上越|富山|野々市|大町|蒲郡|四日市|姫路|大和郡山|廿日市|下松|岩国|田川|大村)市|.+?郡(?:玉村|大町|.+?)[町村]|.+?市.+?区|.+?[市区町村])`;
+    reg += `(\\D+)`;
+    reg += `(.*)`;
+
+    const match = value.match(reg);
+    if (!match || (match && match[4] === '')) {
+      errors.push(ErrorMessage.InvalidAddress);
+    }
+
     FormValidator.changeErrorState(prop, errors, this.props.error);
     FormValidator.changeUiState(prop, value, this.props.ui);
   }
