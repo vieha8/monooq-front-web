5aea4a4a64891c8e35f548bc832ed9e9f9c5b32c
Masaya Kudo <msykd16@gmail.com>
2018-04-14 00:14:57 +0900

    メッセージ分析用管理画面開発着手

---
+++ admin/components/Layout.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import React from 'react';
+import Header from './Header';
+import Head from 'next/head';
+
+const Container = styled.div`
+  margin: 20,
+  padding: 20,
+`;
+
+const Layout = (props) => (
+  <Container>
+    <Head>
+      <title>MonooQ Admin</title>
+      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css" />
+    </Head>
+    {props.children}
+  </Container>
+);
+
+export default Layout;
