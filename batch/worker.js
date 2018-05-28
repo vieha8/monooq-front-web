ae4b509eaed3e73a158ec5fe061a6a4cf34cae5a
Masaya Kudo <msykd16@gmail.com>
2018-05-28 19:41:39 +0900

    メッセージ返信率集計バッチ実装

--- batch/worker.js
+++ batch/worker.js
@@ -1,4 +1,67 @@
 import mysql from 'promise-mysql';
+import firebase from 'firebase';
+import firestore from '../admin/helpers/firestore';
+require('firebase/firestore');
+
+try {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAOzkRrYFUdzQ2kuwMyTjrjcsbhKOfuQVo',
+    authDomain: 'monooq-prod.firebaseapp.com',
+    databaseURL: 'https://monooq-prod.firebaseio.com',
+    projectId: 'monooq-prod',
+    storageBucket: 'monooq-prod.appspot.com',
+    messagingSenderId: '569699475393',
+  });
+} catch (err) {
+  if (!/already exists/.test(err.message)) {
+    console.error('Firebase initialization error', err.stack);
+  }
+}
+
+const getMessageRate = async () => {
+  const rooms = await firestore.collection('rooms').get();
+
+  const messageCounts = {};
+
+  const zeroMessageUsers = [];
+
+  const res = await Promise.all(
+    rooms.docs.map(async room => {
+      const roomId = room.id;
+      const messages = await firestore
+        .collection('rooms')
+        .doc(roomId)
+        .collection('messages')
+        .get();
+      const messageCount = messages.docs.length;
+
+      let key = messageCount;
+      if (messageCount > 1) {
+        key = 2;
+      }
+
+      if (messageCount === 0) {
+        zeroMessageUsers.push(room.data().userId1);
+      }
+
+      if (messageCounts[key]) {
+        messageCounts[key] += 1;
+      } else {
+        messageCounts[key] = 1;
+      }
+    }),
+  );
+
+  const allRoomCount = res.length;
+
+  zeroMessageUsers.sort((a, b) => {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  });
+
+  return messageCounts[2] / (allRoomCount - messageCounts[0]);
+};
 
 const aggregateKPI = async () => {
   const sql = await mysql.createConnection({
@@ -9,14 +72,14 @@ const aggregateKPI = async () => {
     password: '5Fb9N7Vu',
   });
 
-  const rows = await sql.query('SELECT id, name FROM users');
-  console.log(rows.length);
+  const messageRate = await getMessageRate();
+  const q = 'INSERT INTO kpi_message_response_rate(date, rate) VALUES(CURDATE(), ?)';
+  await sql.query(q, [messageRate]);
 
   // TODO デイリーの新規登録ユーザー数
   // TODO デイリーの新規登録スペース数
   // TODO デイリーの新規成約数
   // TODO デイリーの新規リクエスト数
-  // TODO デイリーの初回メッセージ返信率
 
   sql.end();
   console.log('aggregate KPI Success!');
