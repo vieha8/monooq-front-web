9431a22abf5aa8a0e2bb129d2bb40ab9c22b6cf8
imuuzak <kazuumi.hori@spacemarket.co.jp>
2018-06-17 00:12:49 +0900

    静的ページにヘッダ追加

---
+++ src/components/atomic/LV1/StaticPageContent/index.jsx
@@ -0,0 +1,11 @@
+// @flow
+
+import styled from 'styled-components';
+import { Dimens } from 'variables';
+import { media } from 'helpers/style/media-query';
+
+const Container = styled.div`
+  margin-top: ${Dimens.huge}px;
+`;
+
+export default Container;
