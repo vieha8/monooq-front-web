5acd25f56b7e0fab69c78710dfeca2179936c805
imuuzak <kazuumi.hori@spacemarket.co.jp>
2018-06-17 00:26:25 +0900

    remove import

--- src/components/atomic/LV1/StaticPageContent/index.jsx
+++ src/components/atomic/LV1/StaticPageContent/index.jsx
@@ -2,7 +2,6 @@
 
 import styled from 'styled-components';
 import { Dimens } from 'variables';
-import { media } from 'helpers/style/media-query';
 
 const Container = styled.div`
   margin-top: ${Dimens.huge}px;
