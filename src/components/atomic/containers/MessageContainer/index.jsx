59e2eaa0a5b295e09c97cd38f6c898d85e2aef93
Masaya Kudo <msykd16@gmail.com>
2018-06-18 17:17:07 +0900

    見積もりメッセージの名前表記不具合修正

--- src/components/atomic/containers/MessageContainer/index.jsx
+++ src/components/atomic/containers/MessageContainer/index.jsx
@@ -23,6 +23,11 @@ type PropTypes = {
   },
   room: Array<{
     id: string,
+    space: {
+      Host: {
+        Name: string,
+      },
+    },
     user: {
       ID: number,
       ImageUrl: string,
@@ -129,7 +134,7 @@ class InboxContainer extends Component<PropTypes, State> {
           const { startDate, endDate, price, requestId } = message;
           return {
             estimate: {
-              name: room.user.Name,
+              name: room.space.Host.Name,
               beginAt: startDate.toDate(),
               endAt: endDate.toDate(),
               price,
