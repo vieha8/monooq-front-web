a17518826d41795aeb2dedfc127bb9f65a98f1d8
Masaya Kudo <msykd16@gmail.com>
2018-07-12 18:59:33 +0900

    データ修正

---
+++ src/components/Top/pickup.js
@@ -0,0 +1,309 @@
+import Path from '../../config/path';
+import { Colors } from '../../variables';
+
+// 特徴でピックアップ
+export const PickupFeatureSpaceList = [
+  {
+    link: Path.space(1255),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2Fdefault.png?alt=media&token=e36437c2-778c-44cf-a701-2d4c8c3e0363',
+      name: 'Keisuke',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F1255%2F1528249761350.jpg?alt=media&token=cc93b081-61ee-493d-ad09-9a7465058eb7',
+      price: '5,900',
+      area: '佐賀県鳥栖市',
+      description: 'セキュリティ対策OK!',
+      color: Colors.pickup1,
+    },
+  },
+  {
+    link: Path.space(1239),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2F2614%2Fprofile%2F1527860044162.jpg?alt=media&token=4e50ebbc-ee17-4cac-be49-0d38fed80334',
+      name: 'IWGP王者',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F1239%2F1527823413880.jpg?alt=media&token=8c58f3be-7744-47ae-ae04-97717f3e1df1',
+      price: '7,000',
+      area: '宮崎県宮崎市',
+      description: '業務用品もおまかせ！',
+      color: Colors.pickup2,
+    },
+  },
+  {
+    link: Path.space(1152),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2Fdefault.png?alt=media&token=e36437c2-778c-44cf-a701-2d4c8c3e0363',
+      name: 'SHO',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F1152%2F1525593802800.jpg?alt=media&token=4961b5f4-323a-4fd3-8f65-a0df3a7be0bb',
+      price: '8,000',
+      area: '北海道虻田郡ニセコ町',
+      description: 'レジャー用品なら!',
+      color: Colors.pickup3,
+    },
+  },
+  {
+    link: Path.space(801),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2Fdefault.png?alt=media&token=e36437c2-778c-44cf-a701-2d4c8c3e0363',
+      name: 'れも吉',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F801%2F1525561007790.jpg?alt=media&token=90f41359-70e8-4d3a-8bf7-dd06ef640c80',
+      price: '7,000',
+      area: '埼玉県羽生市',
+      description: '集荷も相談できる!',
+      color: Colors.pickup4,
+    },
+  },
+  {
+    link: Path.space(568),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2F1192%2Fprofile%2F1522225975550.jpg?alt=media&token=a4c5ea70-e85d-4ae8-b2fc-df621b3cee6a',
+      name: 'himitsu',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F568%2F1528321575825.jpg?alt=media&token=3cf00a11-337a-4295-b6b6-ec8a0cece859',
+      price: '3,500',
+      area: '神奈川県大磯町',
+      description: '大磯ロングビーチ近く！',
+      color: Colors.pickup1,
+    },
+  },
+  {
+    link: Path.space(570),
+    user: {
+      image:
+        'https://s3-ap-northeast-1.amazonaws.com/monooq/uploads/user/image/421/thumb_a464e336-ba76-4544-8bdb-00a85d08ae1e.jpg',
+      name: 'Kenta',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F570%2F1522563759482.jpg?alt=media&token=315cf44a-fa87-4f5e-9bdb-5965904e9317',
+      price: '5,000',
+      area: '京都府京都市',
+      description: '京都で少量を預けるなら',
+      color: Colors.pickup2,
+    },
+  },
+  {
+    link: Path.space(408),
+    user: {
+      image:
+        'https://s3-ap-northeast-1.amazonaws.com/monooq/uploads/user/image/908/thumb_b3b072ae-a07e-4e71-8832-3d35b9e914b7.jpg',
+      name: 'shinzato kazuma',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F408%2F1528614900673.jpg?alt=media&token=26b24a81-7d8c-41c3-8090-bed37530d573',
+      price: '5,000',
+      area: '埼玉県越谷市',
+      description: 'フローリングのお部屋で',
+      color: Colors.pickup3,
+    },
+  },
+  {
+    link: Path.space(316),
+    user: {
+      image:
+        'https://s3-ap-northeast-1.amazonaws.com/monooq/uploads/user/image/702/thumb_22b5655c-64fe-49db-a893-5923e694e3ff.jpg',
+      name: 'ＣＨＩＫＡＫＯ',
+    },
+    space: {
+      image:
+        'https://s3-ap-northeast-1.amazonaws.com/monooq/uploads/place_image/id/325/4071ab3e-67e2-4269-8fb5-c378cf01e026.jpg',
+      price: '5,000',
+      area: '福岡県博多市',
+      description: 'キーボックスで管理！',
+      color: Colors.pickup4,
+    },
+  },
+];
+
+// スタッフのおすすめ
+export const PickupStaffSpaceList = [
+  {
+    large: true,
+    link: Path.space(1222),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2F2575%2Fprofile%2F1527893789719.jpg?alt=media&token=a9343765-97f5-484f-8872-2b4be94eb079',
+      name: 'Iden',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F1222%2F1527989916929.jpg?alt=media&token=569b5eda-a504-408a-8db2-01cf0e4b5dc4',
+      price: '25,000',
+      area: '東京都豊島区',
+      description:
+        '広ーいスペースはなんと元銭湯！なかなか保管できない大きくて大量の荷物にもおすすめ。',
+      color: Colors.pickup1,
+    },
+  },
+  {
+    large: true,
+    link: Path.space(663),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2F1499%2Fprofile%2F1522547753907.jpg?alt=media&token=7620f7ba-113e-4aa0-b9ed-cca7903fe432',
+      name: 'とっと',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F663%2F1529300059903.jpg?alt=media&token=1fffd259-f498-4fe3-b68c-6ab7f525d97c',
+      price: '3,000',
+      area: '東京都杉並区',
+      description:
+        '大きな荷物も大丈夫！軽トラックをお持ちのホストさんなので、搬入の相談をしてみては？',
+      color: Colors.pickup2,
+    },
+  },
+  {
+    large: true,
+    link: Path.space(581),
+    user: {
+      image:
+        'https://s3-ap-northeast-1.amazonaws.com/monooq/uploads/user/image/936/thumb_694376f3-daba-4d07-b6b8-3e02a4e7fac3.jpg',
+      name: 'your trunk room',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F581%2F1522855347035.jpg?alt=media&token=c656edc7-fa3d-43e2-a567-1e4b9620daca',
+      price: '5,000',
+      area: '東京都板橋区',
+      description:
+        'ダンボールひとつから対応してくれます。小型や少量でちょっとだけ預けたい時にどうぞ！',
+      color: Colors.pickup3,
+    },
+  },
+  {
+    link: Path.space(1029),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2F2279%2Fprofile%2F1530682528880.png?alt=media&token=cba34eb2-d03f-4c0a-9886-3f72a2b67dc6',
+      name: 'Mickey',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F1029%2F1530682249725.jpg?alt=media&token=1187bd20-ebbc-49b6-9bc8-f0ed3645dfe5',
+      price: '5,000',
+      area: '東京都中野区',
+      description:
+        '中野で1部屋まるごと6畳スペース。管理人さん常駐・オートロックのマンションで安心！',
+      color: Colors.pickup3,
+    },
+  },
+];
+
+// 全国の物置きスペース
+export const PickupAreaSpaceList = [
+  {
+    link: Path.space(1222),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2F2575%2Fprofile%2F1527893789719.jpg?alt=media&token=a9343765-97f5-484f-8872-2b4be94eb079',
+      name: 'Iden',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F1222%2F1527989916929.jpg?alt=media&token=569b5eda-a504-408a-8db2-01cf0e4b5dc4',
+      price: '25,000',
+      area: '池袋本町',
+      description: '東京都',
+      color: Colors.pickup1,
+    },
+  },
+  {
+    link: Path.space(663),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2F1499%2Fprofile%2F1522547753907.jpg?alt=media&token=7620f7ba-113e-4aa0-b9ed-cca7903fe432',
+      name: 'とっと',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F663%2F1529300059903.jpg?alt=media&token=1fffd259-f498-4fe3-b68c-6ab7f525d97c',
+      price: '3,000',
+      area: '高円寺南',
+      description: '高円寺',
+      color: Colors.pickup2,
+    },
+  },
+  {
+    link: Path.space(1255),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2Fdefault.png?alt=media&token=e36437c2-778c-44cf-a701-2d4c8c3e0363',
+      name: 'Keisuke',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F1255%2F1528249761350.jpg?alt=media&token=cc93b081-61ee-493d-ad09-9a7465058eb7',
+      price: '5,900',
+      area: '蔵上町',
+      description: '佐賀県',
+      color: Colors.pickup1,
+    },
+  },
+  {
+    link: Path.space(1239),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2F2614%2Fprofile%2F1527860044162.jpg?alt=media&token=4e50ebbc-ee17-4cac-be49-0d38fed80334',
+      name: 'IWGP王者',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F1239%2F1527823413880.jpg?alt=media&token=8c58f3be-7744-47ae-ae04-97717f3e1df1',
+      price: '7,000',
+      area: '旭二丁目',
+      description: '宮崎県',
+      color: Colors.pickup2,
+    },
+  },
+  {
+    link: Path.space(1152),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2Fdefault.png?alt=media&token=e36437c2-778c-44cf-a701-2d4c8c3e0363',
+      name: 'SHO',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F1152%2F1525593802800.jpg?alt=media&token=4961b5f4-323a-4fd3-8f65-a0df3a7be0bb',
+      price: '8,000',
+      area: 'ニセコ町',
+      description: 'ニセコ',
+      color: Colors.pickup3,
+    },
+  },
+  {
+    link: Path.space(801),
+    user: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fusers%2Fdefault.png?alt=media&token=e36437c2-778c-44cf-a701-2d4c8c3e0363',
+      name: 'れも吉',
+    },
+    space: {
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/img%2Fspaces%2F801%2F1525561007790.jpg?alt=media&token=90f41359-70e8-4d3a-8bf7-dd06ef640c80',
+      price: '7,000',
+      area: '与兵エ新田',
+      description: 'さいたま',
+      color: Colors.pickup4,
+    },
+  },
+];
