f8a56b7a96cf884ce9b8eda22c82375698d488eb
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2018-09-05 19:55:12 +0900

    詳細箇所以外の実装完了

--- src/components/atomic/LV3/ConciergeRequest/index.jsx
+++ src/components/atomic/LV3/ConciergeRequest/index.jsx
@@ -1,21 +1,62 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import { DefaultContainer } from 'components/Shared';
 import InputForm from 'components/atomic/LV2/InputForm';
 import SelectForm from 'components/atomic/LV2/SelectForm';
 import InputSchedule from 'components/atomic/LV2/Estimate/InputSchedule';
 import Button from 'components/atomic/LV1/Button';
-import { Dimens, FontSizes } from 'variables';
-import { H2 } from 'components/atomic/LV1/Headline';
+import { Colors, Dimens, FontSizes } from 'variables';
+import { media } from 'helpers/style/media-query';
+import logoAppliv from 'images/logo-appliv@2x.png';
+import logoAscii from 'images/logo-ascii@2x.png';
+import logoBridge from 'images/logo-bridge@2x.png';
+import logoCnet from 'images/logo-cnet@2x.png';
+import logoLifehacker from 'images/logo-lifehacker@2x.png';
+import logoTechcrunch from 'images/logo-techcrunch@2x.png';
+import logoTechable from 'images/logo-techable@2x.png';
 
 const Row = styled.div`
+  max-width: ${Dimens.fixedWidthPc}px;
   &:not(:first-child) {
-    margin: ${Dimens.large}px 0;
+    margin: ${Dimens.large}px auto;
   }
+  ${media.phone`
+    padding: 0 ${Dimens.medium}px;
+  `};
 `;
 
-const Description = styled.div`
-  font-size: ${FontSizes.medium}px;
-  line-height: 150%;
+const TitleForm = styled.div`
+  width: 100%;
+  padding: 25px 0;
+  text-align: center;
+  color: ${Colors.white};
+  background-color: ${Colors.brandPrimary};
+  ${media.phone`
+    padding: 15px 0;
+    margin: 5px auto;
+  `};
+`;
+
+const MainTitle = styled.p`
+  width: 100%;
+  margin: 10px auto;
+  text-align: center;
+  color: ${Colors.white};
+  font-size: ${FontSizes.medium3}px;
+  font-weight: 900;
+  ${media.phone`
+    font-size: ${FontSizes.medium1}px;
+  `};
+`;
+
+const SubTitle = styled.p`
+  width: 100%;
+  text-align: center;
+  color: ${Colors.white};
+  font-size: ${FontSizes.medium2}px;
+  ${media.phone`
+    font-size: ${FontSizes.medium}px;
+  `};
 `;
 
 const sizeList = [
@@ -24,18 +65,108 @@ const sizeList = [
   { key: 3, value: '約3畳', text: '約3畳(546cm x 273cm)' },
 ];
 
+const ButtonSubmit = styled.div`
+  width: 100%;
+  max-width: 375px;
+  margin: 90px auto 40px;
+  padding: 25px 10px;
+  text-align: center;
+  font-size: ${FontSizes.large}px;
+  font-weight: 900;
+  color: #ffffff;
+  background-color: ${Colors.brandPrimary};
+  cursor: pointer;
+  border-radius: 5px;
+  ${media.phone`
+    width: 90%;
+    margin: 45px auto 20px;
+    font-size: ${FontSizes.medium2}px;
+  `};
+`;
+
+const StyledDefaultContainer = styled(DefaultContainer)`
+  padding-top: 80px;
+  padding-bottom: 80px;
+  color: ${Colors.darkGray1};
+  ${media.phone`
+    padding-top: 40px;
+    padding-bottom: 40px;
+  `};
+`;
+const MediaLineupContainer = styled(StyledDefaultContainer)`
+  padding-top: 40px;
+  padding-bottom: 40px;
+`;
+const LineupTitle = styled.div`
+  font-size: ${FontSizes.medium1}px;
+  margin-bottom: 40px;
+`;
+const LineupList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: left;
+`;
+const LineupItem = styled.li`
+  height: 26px;
+  margin-bottom: 20px;
+  margin-right: 40px;
+  ${media.phone`
+    height: 16px;
+    margin-right: 10px;
+  `};
+`;
+const LineupImage = styled.img`
+  height: 100%;
+`;
+const MediaLineup = () => (
+  <MediaLineupContainer>
+    <LineupTitle>メディア掲載</LineupTitle>
+    <LineupList>
+      <LineupItem>
+        <a href="https://japan.cnet.com/" target="_blank" rel="noopener noreferrer">
+          <LineupImage src={logoCnet} alt="CNET Japan" />
+        </a>
+      </LineupItem>
+      <LineupItem>
+        <a href="http://ascii.jp/" target="_blank" rel="noopener noreferrer">
+          <LineupImage src={logoAscii} alt="ASCII.jp" />
+        </a>
+      </LineupItem>
+      <LineupItem>
+        <a href="http://thebridge.jp/" target="_blank" rel="noopener noreferrer">
+          <LineupImage src={logoBridge} alt="THE BRIDGE" />
+        </a>
+      </LineupItem>
+      <LineupItem>
+        <a href="https://mag.app-liv.jp/" target="_blank" rel="noopener noreferrer">
+          <LineupImage src={logoAppliv} alt="Appliv" />
+        </a>
+      </LineupItem>
+      <LineupItem>
+        <a href="https://www.lifehacker.jp/" target="_blank" rel="noopener noreferrer">
+          <LineupImage src={logoLifehacker} alt="lifehacker" />
+        </a>
+      </LineupItem>
+      <LineupItem>
+        <a href="https://jp.techcrunch.com/" target="_blank" rel="noopener noreferrer">
+          <LineupImage src={logoTechcrunch} alt="TechCrunch" />
+        </a>
+      </LineupItem>
+      <LineupItem>
+        <a href="https://techable.jp" target="_blank" rel="noopener noreferrer">
+          <LineupImage src={logoTechable} alt="TECHABLE" />
+        </a>
+      </LineupItem>
+    </LineupList>
+  </MediaLineupContainer>
+);
+
 export default props => (
   <Fragment>
-    <Description>
-      <H2>モノオクコンシェルジュとは?</H2>
-      モノオクコンシェルジュは、あなたのお荷物の保管スペース探しをお手伝いするサービスです!<br />
-      スペース検索で理想のスペースが見つからない、そもそも探し方がよくわからない、、そんな方にオススメです。<br />
-      いただいた条件から合致するスペースを探し、ご提案させていただきます。<br />
-      下記のフォームより、お気軽にお問い合わせください。<br />
-    </Description>
-    <Row>
-      <H2>ご相談フォーム</H2>
-    </Row>
+    <TitleForm>
+      <MainTitle>お申込みフォーム</MainTitle>
+      <SubTitle>まずはお気軽にご相談ください</SubTitle>
+    </TitleForm>
     <Row>
       <InputForm
         label="預けたい地域"
@@ -90,7 +221,8 @@ export default props => (
         placeholder="example@monooq.com"
       />
     </Row>
-    <Button
+    <ButtonSubmit>送信する</ButtonSubmit>
+    {/* <Button
       primary
       fill={1}
       disabled={props.buttonDisabled}
@@ -98,6 +230,7 @@ export default props => (
       loading={props.buttonLoading}
     >
       送信
-    </Button>
+    </Button> */}
+    <MediaLineup />
   </Fragment>
 );
