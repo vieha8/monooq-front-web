2c3b712e28c7cc99000a1ba0091b137e005186c3
Masaya Kudo <msykd16@gmail.com>
2018-09-17 02:16:27 +0900

    トークンの期限チェックを削除

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -5,17 +5,11 @@ import { authActions } from 'redux/modules/auth';
 class AuthContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.props.dispatch(authActions.checkLogin());
+    const { dispatch } = props;
+    dispatch(authActions.checkLogin());
     const token = localStorage.getItem('token');
-    if (token) {
-      const { Expire } = JSON.parse(token);
-      const now = parseInt(Date.now() / 1000, 10);
-      if (Expire < now) {
-        // TODO 本来有効期限切れの場合はRefreshToken叩く
-        this.props.dispatch(authActions.tokenGenerate());
-      }
-    } else {
-      this.props.dispatch(authActions.tokenGenerate());
+    if (!token) {
+      dispatch(authActions.tokenGenerate());
     }
   }
 
