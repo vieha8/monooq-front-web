c08a0041f364f3cc19b590f9cde53586ef5b0ca3
Masaya Kudo <msykd16@gmail.com>
2018-09-20 12:35:20 +0900

    rendertron用ディレクトリ修正

---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const rendertron = require('rendertron-middleware');
+
+const PORT = process.env.PORT || 3000;
+
+// Multi-process to utilize all CPU cores.
+if (cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
+
+} else {
+  const app = express();
+
+  app.use(rendertron.makeMiddleware({
+    proxyUrl: 'http://ec2-13-230-42-134.ap-northeast-1.compute.amazonaws.com/render',
+  }));
+
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  });
+
+  app.listen(PORT, function () {
+    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+  });
+}
