e85d4f97cafc74931ec906c28e9c7d8f814468dc
Masaya Kudo <msykd16@gmail.com>
2018-10-11 23:03:29 +0900

    スペース詳細のスペース画像にimgix導入

--- react-ui/src/components/atomic/LV1/SlideImage/index.jsx
+++ react-ui/src/components/atomic/LV1/SlideImage/index.jsx
@@ -10,13 +10,35 @@ type PropTypes = {
   }>,
 };
 
-export default (props: PropTypes) => (
-  <ImageGallery
-    items={props.images.map(image => ({
-      original: image.original || '',
-      thumbnail: image.thumbnail || '',
+const convertImageUrl = images => {
+  return images.map(({ original }) => {
+    let storageUrl = 'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/';
+    let imgixUrl = 'https://monooq.imgix.net/';
+
+    if (original.indexOf('monooq-dev.appspot.com') > -1) {
+      storageUrl = 'https://firebasestorage.googleapis.com/v0/b/monooq-dev.appspot.com/o/';
+      imgixUrl = 'https://monooq-dev.imgix.net/';
+    }
+
+    let replaceUrl = original.replace(storageUrl, imgixUrl) + '&fit=crop&w=540&max-h=540';
+
+    if (original.indexOf('s3-ap-northeast-1') > -1) {
+      storageUrl = 'https://s3-ap-northeast-1.amazonaws.com/monooq/';
+      imgixUrl = 'https://monooq-s3.imgix.net/';
+      replaceUrl = original.replace(storageUrl, imgixUrl) + '?fit=crop&w=540&max-h=540';
+    }
+
+    return {
+      original: replaceUrl || '',
+      thumbnail: replaceUrl || '',
       bulletClass: 'space_image_gallery_bullet',
-    }))}
+    };
+  });
+};
+
+export default ({ images }: PropTypes) => (
+  <ImageGallery
+    items={convertImageUrl(images)}
     showThumbnails={false}
     showPlayButton={false}
     showFullscreenButton={false}
