fe635c10e2e3ecc16aa758017c99b53ef363ac44
Masaya Kudo <msykd16@gmail.com>
2018-11-27 15:20:08 +0900

    検索フォームの不要な変数等削除

--- react-ui/src/components/atomic/containers/SearchConditionContainer/index.jsx
+++ react-ui/src/components/atomic/containers/SearchConditionContainer/index.jsx
@@ -2,22 +2,13 @@
 
 import React, { Component } from 'react';
 import Path from 'config/path';
-
-import { uiActions } from 'redux/modules/ui';
-import { spaceActions } from 'redux/modules/space';
-
 import MenuPageTemplate from 'components/atomic/templates/MenuPageTemplate';
 import ServiceMenu from 'components/atomic/containers/ServiceMenuContainer';
 import Header from 'components/atomic/containers/Header';
 import SearchCondition from 'components/atomic/LV3/SearchCondition';
-
-import { ErrorMessage } from 'strings';
-
-import { checkLogin, checkAuthState, mergeAuthProps } from '../AuthRequired';
 import connect from '../connect';
 
 type PropTypes = {
-  dispatch: Function,
   history: {
     push: Function,
   },
@@ -29,21 +20,14 @@ type PropTypes = {
 class SearchConditionContainer extends Component<PropTypes> {
   constructor(props) {
     super(props);
-
-    checkLogin(this.props);
-
-    const { dispatch, space } = this.props;
-
-    dispatch(spaceActions.prepareUpdateSpace());
-
     this.state = {
-      keyword: space.Keyword || '',
-      prefCode: space.PrefCode || 0,
-      priceMin: space.PriceMin || '',
-      priceMax: space.PriceMax || '',
-      type: space.Type || 0,
-      isFurniture: space.IsFurniture || false,
-      receiptType: space.ReceiptType || 0,
+      keyword: '',
+      prefCode: 0,
+      priceMin: '',
+      priceMax: '',
+      type: 0,
+      isFurniture: false,
+      receiptType: 0,
       error: {},
     };
   }
@@ -55,9 +39,11 @@ class SearchConditionContainer extends Component<PropTypes> {
   onClickSearch: Function;
 
   onClickSearch = () => {
+    const { history } = this.props;
     const { keyword, prefCode, priceMin, priceMax, type, isFurniture, receiptType } = this.state;
-
     console.log(keyword, prefCode, priceMin, priceMax, type, isFurniture, receiptType);
+
+    history.push(Path.search());
   };
 
   handleChangeUI: Function;
@@ -72,7 +58,6 @@ class SearchConditionContainer extends Component<PropTypes> {
   };
 
   render() {
-    const { space } = this.props;
     const {
       keyword,
       prefCode,
@@ -90,7 +75,6 @@ class SearchConditionContainer extends Component<PropTypes> {
         headline="検索"
         leftContent={
           <SearchCondition
-            edit={space.ID}
             keyword={keyword}
             keywordErrors={error.keyword}
             onChangeKeyword={v => this.handleChangeUI('keyword', v)}
@@ -119,12 +103,4 @@ class SearchConditionContainer extends Component<PropTypes> {
   }
 }
 
-const mapStateToProps = state =>
-  mergeAuthProps(state, {
-    space: state.ui.space || {},
-  });
-
-export default connect(
-  SearchConditionContainer,
-  mapStateToProps,
-);
+export default connect(SearchConditionContainer);
