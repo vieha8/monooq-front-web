8e1a4f8ae14a2252fed58bb60936d4fc3d6b0b77
Masaya Kudo <msykd16@gmail.com>
2018-12-01 17:41:12 +0900

    Imgixコンバートロジック修正

--- react-ui/src/helpers/imgix.js
+++ react-ui/src/helpers/imgix.js
@@ -29,18 +29,21 @@ export const convertBaseUrl = imgixUrl => {
     const storageUrl = 'https://firebasestorage.googleapis.com/v0/b/monooq-dev.appspot.com/o/';
     replaceUrl = imgixUrl.replace('https://monooq-dev.imgix.net/', storageUrl);
     replaceUrl = replaceUrl.replace('&fit=crop&max-w=540&max-h=290&format=auto', '');
+    replaceUrl = replaceUrl.replace('&fit=crop&w=240&max-h=180&format=auto', '');
   }
 
   if (imgixUrl.indexOf('monooq.imgix.net') > -1) {
     const storageUrl = 'https://firebasestorage.googleapis.com/v0/b/monooq-prod.appspot.com/o/';
     replaceUrl = imgixUrl.replace('https://monooq.imgix.net/', storageUrl);
     replaceUrl = replaceUrl.replace('&fit=crop&max-w=540&max-h=290&format=auto', '');
+    replaceUrl = replaceUrl.replace('&fit=crop&w=240&max-h=180&format=auto', '');
   }
 
   if (imgixUrl.indexOf('monooq-s3.imgix.net') > -1) {
     const storageUrl = 'https://s3-ap-northeast-1.amazonaws.com/monooq/';
     replaceUrl = imgixUrl.replace('https://monooq-s3.imgix.net/', storageUrl);
     replaceUrl = replaceUrl.replace('?fit=crop&max-w=540&max-h=290&format=auto', '');
+    replaceUrl = replaceUrl.replace('&fit=crop&w=240&max-h=180&format=auto', '');
   }
 
   return replaceUrl;
