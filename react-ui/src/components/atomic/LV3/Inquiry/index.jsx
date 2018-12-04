83125d576f33c8c6ba96c3acc99bbba9afb55a2c
Masaya Kudo <msykd16@gmail.com>
2018-12-04 18:43:40 +0900

    未ログイン時、問い合わせページに退会導線を表示しないように修正

--- react-ui/src/components/atomic/LV3/Inquiry/index.jsx
+++ react-ui/src/components/atomic/LV3/Inquiry/index.jsx
@@ -1,13 +1,11 @@
 // @flow
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import InlineText from 'components/atomic/LV1/InlineText';
 import TextLink from 'components/atomic/LV1/TextLink';
 import { Dimens } from 'variables';
 import Path from 'config/path';
-
-const Row = styled.div``;
 
 const Text = styled.div`
   margin-top: ${Dimens.medium}px;
@@ -17,21 +15,25 @@ const UnsubscribeText = styled.div`
   margin-top: ${Dimens.large}px;
 `;
 
-export default () => (
-  <div>
-    <Row>
-      <Text>
-        <InlineText.Base>
-          サービスの不明点・お困りのことがあれば、モノオクカスタマーサポートまでお寄せください。
-          <br />
-          現在、お問い合わせは右下の赤いアイコンから承っております。こちらよりご連絡ください。
-          <br />
-          お問い合わせ内容を送信後、メールアドレスを入力いただくと翌営業日を目安にご連絡致します。
-        </InlineText.Base>
-      </Text>
-    </Row>
-    <UnsubscribeText>
-      <TextLink to={Path.unsubscribe()}>退会はこちら</TextLink>
-    </UnsubscribeText>
-  </div>
+type PropTypes = {
+  isLogin: boolean,
+};
+
+export default ({ isLogin }: PropTypes) => (
+  <Fragment>
+    <Text>
+      <InlineText.Base>
+        サービスの不明点・お困りのことがあれば、モノオクカスタマーサポートまでお寄せください。
+        <br />
+        現在、お問い合わせは右下の赤いアイコンから承っております。こちらよりご連絡ください。
+        <br />
+        お問い合わせ内容を送信後、メールアドレスを入力いただくと翌営業日を目安にご連絡致します。
+      </InlineText.Base>
+    </Text>
+    {isLogin && (
+      <UnsubscribeText>
+        <TextLink to={Path.unsubscribe()}>退会はこちらから</TextLink>
+      </UnsubscribeText>
+    )}
+  </Fragment>
 );
