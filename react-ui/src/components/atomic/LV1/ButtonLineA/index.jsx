bbc781d405587363b165bf393d2c141c562f99e7
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2019-01-09 20:17:33 +0900

    line＠ボタンコンポーネントalt変数化

--- react-ui/src/components/atomic/LV1/ButtonLineA/index.jsx
+++ react-ui/src/components/atomic/LV1/ButtonLineA/index.jsx
@@ -6,6 +6,7 @@ import { Dimens } from 'variables';
 import ReactGA from 'react-ga';
 
 const imageUrl = 'https://scdn.line-apps.com/n/line_add_friends/btn/ja.png';
+const imageAlt = '友だち追加';
 
 const ButtonLineWrap = styled.a`
   :hover {
@@ -34,11 +35,11 @@ export default (props: PropTypes) => (
     rel="noopener noreferrer"
   >
     {props.story ? (
-      <ButtonLineImage src={imageUrl} alt="友だち追加" />
+      <ButtonLineImage src={imageUrl} alt="imageAlt" />
     ) : (
       <ButtonLineImage
         src={imageUrl}
-        alt={props.reactGACategory}
+        alt={imageAlt}
         onClick={() =>
           ReactGA.event({ category: '`props.reactGACategory`', action: '`props.reactGAAction`' })
         }
