7f5c117686fad01ac8f648ac37beb9973d7c5ad6
Masaya Kudo <m-kudo@monooq.com>
2019-05-14 06:28:08 +0900

    不要コード削除

--- react-ui/src/components/containers/AuthRequired.jsx
+++ react-ui/src/components/containers/AuthRequired.jsx
@@ -2,46 +2,9 @@
 
 import React, { Component } from 'react';
 import { Redirect, withRouter } from 'react-router-dom';
-import { authActions } from 'redux/modules/auth';
 import Path from 'config/path';
-import LoadingPage from 'components/LV3/LoadingPage';
 import { uiActions } from 'redux/modules/ui';
 import { connect } from 'react-redux';
-
-type PropTypes = {
-  isChecking: boolean,
-  isLogin: boolean,
-  dispatch: Function,
-  location: Object,
-};
-
-export function checkLogin(props: PropTypes) {
-  const { dispatch, location } = props;
-  if (!props.isChecking && !props.isLogin) {
-    dispatch(uiActions.setUiState({ redirectPath: location.pathname }));
-    dispatch(authActions.checkLogin());
-  }
-}
-
-export function checkAuthState({ isChecking, isLogin }: PropTypes) {
-  if (isChecking) {
-    return <LoadingPage hideProgress />;
-  }
-
-  if (!isLogin) {
-    return <Redirect to={Path.login()} />;
-  }
-
-  return null;
-}
-
-export function mergeAuthProps(state: Object, props: Object) {
-  return {
-    ...props,
-    isLogin: state.auth.isLogin,
-    isChecking: state.auth.isChecking,
-  };
-}
 
 export default function authRequired(WrappedComponent: Component) {
   class authRequiredComponent extends Component {
