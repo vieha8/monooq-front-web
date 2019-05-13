92b93280775b32564955e693583af0c0b19738a9
Masaya Kudo <m-kudo@monooq.com>
2019-05-14 05:59:58 +0900

    bug fix

--- react-ui/src/components/containers/AuthRequired.jsx
+++ react-ui/src/components/containers/AuthRequired.jsx
@@ -5,13 +5,14 @@ import { Redirect, withRouter } from 'react-router-dom';
 import { authActions } from 'redux/modules/auth';
 import Path from 'config/path';
 import LoadingPage from 'components/LV3/LoadingPage';
-import { uiActions } from '../../redux/modules/ui';
+import { uiActions } from 'redux/modules/ui';
 import { connect } from 'react-redux';
 
 type PropTypes = {
-  dispatch: Function,
   isChecking: boolean,
   isLogin: boolean,
+  dispatch: Function,
+  location: Object,
 };
 
 export function checkLogin(props: PropTypes) {
@@ -57,7 +58,7 @@ export default function authRequired(WrappedComponent: Component) {
       if (!isLogin) {
         return <Redirect to={Path.login()} />;
       }
-      return <WrappedComponent />;
+      return <WrappedComponent {...this.props} />;
     }
   }
 
