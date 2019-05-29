edd762fac8e5bdba615cc72048fab5521c495d0a
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2019-05-29 18:50:09 +0900

    404とシステムエラー画面にモノボーイを配置、レイアウト微調整

--- react-ui/src/components/LV3/NotFound/index.jsx
+++ react-ui/src/components/LV3/NotFound/index.jsx
@@ -2,54 +2,91 @@ import React, { Fragment } from 'react';
 import Path from 'config/path';
 
 import styled from 'styled-components';
-import { Dimens } from 'variables';
+import { media } from 'helpers/style/media-query';
+import { Dimens, FontSizes } from 'variables';
 import DefaultContainer from 'components/LV1/DefaultContainer';
 import Footer from 'components/LV2/Footer';
 import MainTitleContainer from 'components/LV2/StaticMainTitle';
 import RecommendLinkList from 'components/LV2/RecommendLinkList';
 import Text from 'components/LV1/StaticText';
+import Logo from 'components/LV1/Logo';
+
+const WrapContent = styled.div`
+  text-align: center;
+`;
 
 const MessageContainer = styled(DefaultContainer)`
-  margin-bottom: ${Dimens.medium2}px;
+  margin-bottom: ${Dimens.large}px;
+  ${media.phone`
+    margin-bottom: ${Dimens.medium_20}px;
+  `};
+`;
+
+const MonoboyWrap = styled.div`
+  width: 100%;
+  max-width: 200px;
+  margin: 0px auto ${Dimens.small_11}px;
+  padding-top: ${Dimens.medium2_36}px;
+  ${media.phone`
+    max-width: 120px;
+    padding-top: 0px;
+  `};
+`;
+
+const BrStyled = styled.br`
+  display: none;
+  ${media.tablet`
+    display: block;
+  `};
 `;
 
 export default () => (
   <Fragment>
-    <MainTitleContainer
-      mainTitle="ごめんなさい！"
-      mainTitleSub="お探しのページが見つかりません。"
-    />
+    <WrapContent>
+      <MonoboyWrap>
+        <Logo.MonoboyBlack />
+      </MonoboyWrap>
 
-    <MessageContainer>
-      <Text>
-        404エラー
-        <br />
-        ご指定のページは削除されたか、移動した可能性があります。
-      </Text>
-    </MessageContainer>
+      <MainTitleContainer
+        mainTitle="ごめんなさい！"
+        mainTitleSub="お探しのページが見つかりません。"
+        noMarginTop
+        fontSizeSp={FontSizes.medium1}
+      />
 
-    <RecommendLinkList
-      Text="参考のリンク"
-      list={[
-        {
-          text: 'トップページへ戻る',
-          path: '/',
-        },
-        {
-          text: 'ホストになる',
-          path: Path.signUp(),
-        },
-        {
-          text: 'はじめての方へ',
-          path: Path.about(),
-        },
-        {
-          text: 'ヘルプ',
-          path: 'https://help.monooq.com/',
-          blank: '_blank',
-        },
-      ]}
-    />
+      <MessageContainer>
+        <Text fontSizeSp={FontSizes.small_15}>
+          404エラー
+          <br />
+          ご指定のページは削除されたか、
+          <BrStyled />
+          移動した可能性があります。
+        </Text>
+      </MessageContainer>
+
+      <RecommendLinkList
+        Text="参考のリンク"
+        list={[
+          {
+            text: 'トップページへ戻る',
+            path: '/',
+          },
+          {
+            text: 'ホストになる',
+            path: Path.signUp(),
+          },
+          {
+            text: 'はじめての方へ',
+            path: Path.about(),
+          },
+          {
+            text: 'ヘルプ',
+            path: 'https://help.monooq.com/',
+            blank: '_blank',
+          },
+        ]}
+      />
+    </WrapContent>
 
     <Footer />
   </Fragment>
