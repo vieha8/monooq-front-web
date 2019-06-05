bd19c1dff72512af8dae31d19d79ce789a1411b1
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2019-06-05 11:27:23 +0900

    keycode定数の記述を削除

--- react-ui/src/variables/index.js
+++ react-ui/src/variables/index.js
@@ -1,6 +1,5 @@
 export { default as Colors } from './colors';
 export { default as Dimens } from './dimens';
 export { default as FontSizes } from './fontsizes';
-export { default as KeyCode } from './keycode';
 export { default as ZIndexes } from './z-indexes';
 export { default as ErrorMessages } from './errorMessages';
