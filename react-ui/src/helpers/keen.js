e4c16be2eed4d7e31f106f8517e002171bd3062c
Masaya Kudo <m-kudo@monooq.com>
2019-08-20 16:55:07 +0900

    keenトラッキング用汎用関数追加

--- react-ui/src/helpers/keen.js
+++ react-ui/src/helpers/keen.js
@@ -8,3 +8,92 @@ export const keenClient = new KeenTracking({
   projectId: PROJECT_ID,
   writeKey: WRITE_KEY,
 });
+
+const helpers = KeenTracking.helpers;
+const utils = KeenTracking.utils;
+
+const sessionCookie = utils.cookie('keen-monooq');
+if (!sessionCookie.get('keen_uuid')) {
+  sessionCookie.set('keen_uuid', helpers.getUniqueId());
+  sessionCookie.set('first_visited_at', new Date().toISOString());
+  sessionCookie.set('first_referrer', document.referrer);
+}
+
+export const recordEvent = (eventName, body) => {
+  const eventBody = {
+    ...body,
+    env: process.env.NODE_ENV,
+    // eslint-disable-next-line no-template-curly-in-string
+    ip_address: '${keen.ip}',
+    // eslint-disable-next-line no-template-curly-in-string
+    user_agent: '${keen.user_agent}',
+    page: {
+      url: document.location.href,
+    },
+    referrer: {
+      url: document.referrer,
+    },
+    timestamp: new Date().toISOString(),
+    visitor: {
+      uuid: sessionCookie.get('keen_uuid'),
+      first_visited_at: sessionCookie.get('first_visited_at'),
+      first_referrer: sessionCookie.get('first_referrer'),
+    },
+    keen: {
+      addons: [
+        {
+          name: 'keen:ip_to_geo',
+          input: {
+            ip: 'ip_address',
+          },
+          output: 'ip_geo_info',
+        },
+        {
+          name: 'keen:ip_to_geo',
+          input: {
+            ip: 'ip_address',
+          },
+          output: 'ip_geo_info',
+        },
+        {
+          name: 'keen:url_parser',
+          input: {
+            url: 'page.url',
+          },
+          output: 'parsed_page_url',
+        },
+        {
+          name: 'keen:referrer_parser',
+          input: {
+            page_url: 'page.url',
+            referrer_url: 'referrer.url',
+          },
+          output: 'referrer.info',
+        },
+        {
+          name: 'keen:date_time_parser',
+          input: {
+            date_time: 'keen.timestamp',
+          },
+          output: 'timestamp_info',
+        },
+        {
+          name: 'keen:date_time_parser',
+          input: {
+            date_time: 'visitor.first_visited_at',
+          },
+          output: 'visitor.first_visited_info',
+        },
+        {
+          name: 'keen:referrer_parser',
+          input: {
+            page_url: 'page.url',
+            referrer_url: 'visitor.first_referrer',
+          },
+          output: 'visitor.first_referrer_info',
+        },
+      ],
+    },
+  };
+  keenClient.recordEvent(eventName, eventBody);
+};
