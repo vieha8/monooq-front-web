9b093557ac616892ebb18daa77266c3bed7b9e9b
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2019-08-23 22:35:10 +0900

    stories.jsx未追加コンポーネントに当該ファイルを追加

---
+++ react-ui/src/components/LV3/SystemError/stories.jsx
@@ -0,0 +1,25 @@
+// @flow
+
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { Dimens } from 'variables';
+
+import SystemError from './index';
+
+SystemError.displayName = 'SystemError';
+
+storiesOf('Organisms(LV3)/SystemError', module)
+  .addDecorator(story => <MemoryRouter>{story()}</MemoryRouter>)
+  .add(
+    'Normal',
+    withInfo(`
+        ### コンポーネント概要
+        システムエラー
+      `)(() => (
+      <div style={{ padding: `${Dimens.storyBookPadding}` }}>
+        <SystemError />
+      </div>
+    )),
+  );
