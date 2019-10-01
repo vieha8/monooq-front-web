1ab0efa752dd02816ee89ccb144dceea2c40b9ec
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2019-10-01 14:32:08 +0900

    Merge branch 'mod_howtouse' of https://github.com/monooq/monooq-front-web into renewal_201910
    # Conflicts:
    #react-ui/src/components/LV3/HowToUse/index.jsx
    #react-ui/src/components/containers/HowToUseContainer/index.jsx

---
+++ react-ui/src/components/containers/HowToUseContainer/index.jsx
@@ -0,0 +1,13 @@
+// @flow
+
+import React, { Component } from 'react';
+import ContentPageStatic from 'components/hocs/ContentPageStatic';
+import HowToUse from 'components/LV3/HowToUse';
+
+class HowToUseContainer extends Component<*> {
+  render() {
+    return <HowToUse />;
+  }
+}
+
+export default ContentPageStatic(HowToUseContainer);
