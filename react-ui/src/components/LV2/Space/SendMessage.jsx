85162f82817ce586a09fba38aa7d0987ac7f9ef8
shunsuke <babashun1024@gmail.com>
2019-10-06 16:39:58 +0900

    add message link

--- react-ui/src/components/LV2/Space/SendMessage.jsx
+++ react-ui/src/components/LV2/Space/SendMessage.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from 'components/LV1/Forms/Button';
+import TextLink from 'components/LV1/Texts/TextLink';
 import InlineText from 'components/LV1/Texts/InlineText';
 import { Dimens } from 'variables';
 import { media } from 'helpers/style/media-query';
@@ -10,7 +11,7 @@ import { media } from 'helpers/style/media-query';
 const Caption = styled.div`
   display: inline-block;
   text-align: center;
-  margin-top: ${Dimens.small}px;
+  margin-top: ${Dimens.small_10}px;
   margin-left: ${Dimens.medium1}px;
   ${media.phone`
     display: block;
@@ -51,7 +52,16 @@ export default ({ disabled, loading, onClick, onKeyDownButtonMessage }: PropType
       </Button>
     </ButtonWrap>
     <Caption>
-      <InlineText.EmphasisTiny>ご請求はまだ発生しません</InlineText.EmphasisTiny>
+      <TextLink
+        href="https://help.monooq.com/ja/articles/2948108-%E3%83%9B%E3%82%B9%E3%83%88%E3%81%A8%E3%81%AE%E3%82%84%E3%82%8A%E5%8F%96%E3%82%8A%E3%81%AF%E3%81%A9%E3%81%86%E3%82%84%E3%81%A3%E3%81%A6%E9%80%B2%E3%82%81%E3%82%8B%E3%81%AE"
+        fontSize={15}
+        fontsizesp={15}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="gaSpaceDetailBottomFlowLink"
+      >
+        取引の流れをみる
+      </TextLink>
     </Caption>
   </div>
 );
