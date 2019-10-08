486e93f6e8336596b288401909a320584e78ec99
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2019-10-09 01:01:06 +0900

    パンくずリスト実装

---
+++ react-ui/src/components/LV2/Lists/BreadcrumbsList/stories.jsx
@@ -0,0 +1,39 @@
+// @flow
+
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { Dimens } from 'variables';
+
+import BreadcrumbsList from './index';
+
+BreadcrumbsList.displayName = 'BreadcrumbsList';
+
+storiesOf('Molecules(LV2)/Lists/BreadcrumbsList', module)
+  .addDecorator(story => <MemoryRouter>{story()}</MemoryRouter>)
+  .add(
+    'Normal',
+    withInfo(`
+      ### コンポーネント概要
+      パンくずリスト
+    `)(() => (
+      <div style={{ padding: `${Dimens.storyBookPadding}` }}>
+        <BreadcrumbsList
+          breadcrumbsList={[
+            {
+              text: 'TOP',
+              link: '/',
+            },
+            {
+              text: '東京都',
+              link: '/tokyo',
+            },
+            {
+              text: '渋谷区のスペース一覧',
+            },
+          ]}
+        />
+      </div>
+    )),
+  );
