2d9a2292321c3bece4f4de728b64eac8a99fdb7e
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2019-10-10 11:58:29 +0900

    リンクリストの機能追加

--- react-ui/src/components/LV2/Lists/LinkList/index.jsx
+++ react-ui/src/components/LV2/Lists/LinkList/index.jsx
@@ -1,35 +1,68 @@
 // @flow
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { Dimens } from 'variables';
+import { Dimens, Colors } from 'variables';
 
-const Anchor = styled.a`
-  text-decoration: none;
-  :hover {
-    text-decoration: underline;
-  }
+const List = styled.ul``;
+
+const Item = styled.li`
+  line-height: ${Dimens.medium1}px;
+  position: relative;
+  ${props =>
+    props.landscape &&
+    `
+    display: inline-block;
+    &:not(:first-child) {
+      margin-left: ${Dimens.medium_20}px;
+      &:before {
+        content: '|';
+        display: block;
+        position: absolute;
+        left: -${Dimens.small2_15}px;
+      }
+    }
+  `};
 `;
 
-const List = styled.div`
-  line-height: ${Dimens.medium1}px;
+const Anchor = styled.a`
+  ${props => props.color && `color: ${props.color};`}
+  ${props =>
+    props.current &&
+    `
+    color: ${Colors.black};
+    pointer-events: none;
+  `}
+  text-decoration: none;
+  :hover {
+    ${props => props.color && `color: ${props.color};`}
+    opacity: 0.8;
+  }
 `;
 
 type PropTypes = {
   list: Array<{
     text: string,
     path: string,
+    current?: boolean,
   }>,
+  landscape?: boolean,
+  color?: string,
 };
 
-export default ({ list }: PropTypes) => (
-  <Fragment>
+export default ({ list, landscape, color }: PropTypes) => (
+  <List>
     {list.map((item, i) => (
-      <List key={i.toString()}>
-        <Anchor href={item.path} target={item.blank || '_self'}>
+      <Item key={i.toString()} landscape={landscape}>
+        <Anchor
+          href={item.path}
+          target={item.blank || '_self'}
+          color={color}
+          current={item.current}
+        >
           {item.text}
         </Anchor>
-      </List>
+      </Item>
     ))}
-  </Fragment>
+  </List>
 );
--- react-ui/src/components/LV2/Lists/LinkList/stories.jsx
+++ react-ui/src/components/LV2/Lists/LinkList/stories.jsx
@@ -5,40 +5,74 @@ import Path from 'config/path';
 
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { Dimens } from 'variables';
+import { Dimens, Colors } from 'variables';
 
 import LinkList from './index';
 
 LinkList.displayName = 'LinkList';
 
-storiesOf('Molecules(LV2)/Lists/LinkList', module).add(
-  'Normal',
-  withInfo(`
+storiesOf('Molecules(LV2)/Lists/LinkList', module)
+  .add(
+    'Normal',
+    withInfo(`
       ### コンポーネント概要
       リンクリスト
     `)(() => (
-    <div style={{ padding: `${Dimens.storyBookPadding}` }}>
-      <LinkList
-        list={[
-          {
-            text: 'トップページへ戻る',
-            path: '/',
-          },
-          {
-            text: 'ホストになる',
-            path: Path.signUp(),
-          },
-          {
-            text: 'はじめての方へ',
-            path: Path.about(),
-          },
-          {
-            text: 'よくある質問',
-            path: 'https://help.monooq.com/',
-            blank: '_blank',
-          },
-        ]}
-      />
-    </div>
-  )),
-);
+      <div style={{ padding: `${Dimens.storyBookPadding}` }}>
+        <LinkList
+          list={[
+            {
+              text: 'トップページへ戻る',
+              path: '/',
+            },
+            {
+              text: 'ホストになる',
+              path: Path.signUp(),
+            },
+            {
+              text: 'はじめての方へ',
+              path: Path.about(),
+            },
+            {
+              text: 'よくある質問',
+              path: 'https://help.monooq.com/',
+              blank: '_blank',
+            },
+          ]}
+        />
+      </div>
+    )),
+  )
+  .add(
+    'Landscape',
+    withInfo(`
+      ### コンポーネント概要
+      リンクリスト(横並びver)
+      * ■パラメータ
+      * text：リンク名
+      * path：リンク先
+      * current：カレントリンク(リンク非活性になる)
+    `)(() => (
+      <div style={{ padding: `${Dimens.storyBookPadding}` }}>
+        <LinkList
+          list={[
+            {
+              text: '新着順',
+              path: '/',
+              current: true,
+            },
+            {
+              text: 'おすすめ',
+              path: Path.signUp(),
+            },
+            {
+              text: '安い順',
+              path: Path.about(),
+            },
+          ]}
+          landscape
+          color={Colors.brandPrimary}
+        />
+      </div>
+    )),
+  );
