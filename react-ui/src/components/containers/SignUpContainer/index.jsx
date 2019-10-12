d2bd13409aa7be7e9c0301c465237bb5d0f333f7
Masaya Kudo <m-kudo@monooq.com>
2019-10-12 22:51:16 +0900

    ログイン済みの場合signupページにアクセスできない様修正

--- react-ui/src/components/containers/SignUpContainer/index.jsx
+++ react-ui/src/components/containers/SignUpContainer/index.jsx
@@ -3,23 +3,27 @@
 import React, { Component } from 'react';
 import AccountTemplate from 'components/templates/AccountTemplate';
 import Header from 'components/containers/Header';
+import Path from 'config/path';
 import { authActions } from 'redux/modules/auth';
 import RegisterEmail from './RegisterEmail';
 import connect from '../connect';
 
 class SignUpContainer extends Component {
   componentDidMount() {
-    const { dispatch } = this.props;
+    const { dispatch, user, history } = this.props;
+    if (user.id) {
+      history.push(Path.top());
+    }
     dispatch(authActions.initSignup());
   }
 
   render() {
-    const { isSignupFailed } = this.props;
+    const { isSignUpFailed } = this.props;
     return (
       <AccountTemplate
         header={<Header noHeaderButton />}
         form={<RegisterEmail {...this.props} />}
-        err={isSignupFailed}
+        err={isSignUpFailed}
       />
     );
   }
@@ -29,7 +33,7 @@ const mapStateToProps = state => ({
   user: state.auth.user,
   isRegistering: state.auth.isRegistering,
   isLoading: state.user.isLoading,
-  isSignupFailed: state.auth.isSignupFailed,
+  isSignUpFailed: state.auth.isSignupFailed,
   errorMessage: state.auth.errorMessage,
 });
 
