c77bb49618e5bd5dabdbd4f22408da0d5c529b30
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2019-11-27 00:57:54 +0900

    Cardコンポーネント拡張、メッセージCardのSP内padding調整、文言調整

--- react-ui/src/components/LV2/Message/Other.jsx
+++ react-ui/src/components/LV2/Message/Other.jsx
@@ -31,7 +31,7 @@ export default ({ id, image, extension, message, receivedAt }) => (
       </AvatarWrapper>
       <CardWrapper>
         {extension || (
-          <Card block noBorder background={Colors.lightGray1Bg} isPadding={24}>
+          <Card block noBorder background={Colors.lightGray1Bg} isPadding={24} isPaddingSp={14}>
             <InlineText.Base fontSize={15}>{message}</InlineText.Base>
           </Card>
         )}
