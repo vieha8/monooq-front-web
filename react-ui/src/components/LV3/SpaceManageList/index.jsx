02c1a4b03d982ec7f98a651ff7e076651d4da04e
ishizuki.hiroaki <ishizuki.hiroaki@coc-labo.com>
2019-12-17 15:22:13 +0900

    管理スペース一覧の新規追加ボタン実装

--- react-ui/src/components/LV3/SpaceManageList/index.jsx
+++ react-ui/src/components/LV3/SpaceManageList/index.jsx
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
-import SpaceManageListItem from 'components/LV2/Items/SpaceManageListItem';
+import { Link } from 'react-router-dom';
 import { Dimens, Colors } from 'variables';
-import { media } from 'helpers/style/media-query';
+import Path from 'config/path';
+import { media, mediaMin } from 'helpers/style/media-query';
+import SpaceManageListItem from 'components/LV2/Items/SpaceManageListItem';
 
 const Wrap = styled.div`
   display: flex;
@@ -15,15 +17,67 @@ const ListItem = styled.div`
   margin: ${Dimens.small2}px auto;
   padding: ${Dimens.medium1}px ${Dimens.medium1}px 0 ${Dimens.medium1}px;
   background-color: ${Colors.white};
+  ${props =>
+    props.AddSpace &&
+    `
+      position: relative;
+      height: 270px;
+      margin-left: 0;
+      padding: 0;
+      background-color: unset;
+      border: 2px solid ${Colors.brandPrimary};
+      border-style: dashed;
+      color: ${Colors.brandPrimary};
+    `};
+
   &:nth-child(2n) {
     margin-left: ${Dimens.medium1}px;
   }
+
   ${media.tablet`
     width: 100%;
     &:nth-child(2n) {
       margin-left: 0;
     }
   `};
+`;
+
+const LinkStyled = styled(Link)`
+  display: block;
+  width: 100%;
+  height: 100%;
+  color: ${Colors.brandPrimary};
+  &:active {
+    color: ${Colors.brandPrimary};
+    opacity: 0.8;
+  }
+  ${mediaMin.tablet`
+    &:hover {
+      color: ${Colors.brandPrimary};
+      opacity: 0.8;
+    }
+  `};
+`;
+
+const AddIcon = styled.span`
+  position: absolute;
+  top: calc(50% + ${Dimens.small_10}px);
+  left: calc(50% - 95px);
+  font-weight: bold;
+  &::before,
+  &::after {
+    display: block;
+    content: '';
+    position: absolute;
+    top: calc(50% - 50px);
+    left: 50%;
+    width: ${Dimens.xxsmall}px;
+    height: ${Dimens.medium1}px;
+    background: ${Colors.brandPrimary};
+  }
+  &::after {
+    transform: rotate(90deg);
+  }
 `;
 
 export default ({ spaces }) => (
@@ -35,5 +89,10 @@ export default ({ spaces }) => (
         </ListItem>
       </Fragment>
     ))}
+    <ListItem AddSpace>
+      <LinkStyled to={Path.spaceCreate1()}>
+        <AddIcon>新しいスペースを登録する</AddIcon>
+      </LinkStyled>
+    </ListItem>
   </Wrap>
 );
