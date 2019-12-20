b3e24947172f0cc0f32efb0d1d5a7f069824b559
nishisuke <ryoskns+git@gmail.com>
2019-12-20 18:29:58 +0900

    Fix reflect derived state

--- react-ui/src/components/containers/SpaceEdit1Container/index.jsx
+++ react-ui/src/components/containers/SpaceEdit1Container/index.jsx
@@ -158,7 +158,7 @@ class SpaceEdit1Container extends Component {
     const { user, space, dispatch, match } = this.props;
     const { isUpdate, images, title, introduction, sizeType } = this.state;
     const spaceId = match.params.space_id;
-    if (isUpdate && (!space.id || spaceId !== space.id)) {
+    if (isUpdate && (!space.id || parseInt(spaceId, 10) !== space.id)) {
       dispatch(spaceActions.prepareUpdateSpace(spaceId));
       if (space.images && space.images.length === 1 && isImageDefault(space.images[0].imageUrl)) {
         this.setState({ images: [] });
@@ -178,21 +178,47 @@ class SpaceEdit1Container extends Component {
   static getDerivedStateFromProps(nextProps, prevState) {
     const { space } = nextProps;
     if ((space.id && !prevState.id) || space.id !== prevState.id) {
-      const { title, status, introduction, sizeType, tags, images, id } = space;
-      const officialTags = tags.filter(v => v.isOfficial === true);
-      const otherTags = tags.filter(v => v.isOfficial === false);
+      const {
+        title,
+        status,
+        introduction,
+        sizeType,
+        tags,
+        tagList,
+        tagCustomList,
+        images,
+        id,
+      } = space;
+      let newStateTagCustomList = tagCustomList;
+      if (newStateTagCustomList === undefined) {
+        const otherTags = tags.filter(v => v.isOfficial === false);
+        newStateTagCustomList = otherTags.map(v => v.name);
+      }
 
-      const tagList = TagList.map(v => {
-        const isChecked = officialTags.filter(t => v.text === t.name).length > 0;
-        return { ...v, isChecked };
-      });
+      let newStateTagList = tagList;
+      if (newStateTagList === undefined) {
+        const officialTags = tags.filter(v => v.isOfficial === true);
 
-      const tagCustomList = otherTags.map(v => v.name);
+        newStateTagList = TagList.map(v => {
+          const isChecked = officialTags.filter(t => v.text === t.name).length > 0;
+          return { ...v, isChecked };
+        });
+      }
 
       const error = {};
       error.sizeType = checkError('sizeType', sizeType);
 
-      return { title, status, introduction, sizeType, tagList, tagCustomList, images, id, error };
+      return {
+        title,
+        status,
+        introduction,
+        sizeType,
+        tagList: newStateTagList,
+        tagCustomList: newStateTagCustomList,
+        images,
+        id,
+        error,
+      };
     }
     return null;
   }
