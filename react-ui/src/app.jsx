fac7d35ec60d850078ea158c5320e159c433b8c9
Masaya Kudo <m-kudo@monooq.com>
2020-01-17 13:05:34 +0900

    loadableのreload処理追加

--- react-ui/src/app.jsx
+++ react-ui/src/app.jsx
@@ -9,7 +9,9 @@ import createStore, { history } from './redux/store';
 import Routes from './routes';
 import './index.css';
 
-const Error = loadable(() => import('components/containers/Error'));
+const Error = loadable(() =>
+  import('components/containers/Error').catch(() => window.location.reload()),
+);
 
 const store = createStore();
 
