4a1cd770f9dcf9aed576d63c1211e5122dbb58ba
Masaya Kudo <m-kudo@monooq.com>
2020-01-28 01:31:12 +0900

    ErrorModalのhooks化

--- react-ui/src/components/pages/Error.jsx
+++ react-ui/src/components/pages/Error.jsx
@@ -1,52 +1,47 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 import { Modal, Button } from 'semantic-ui-react';
 
-class ErrorModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { open: false };
-  }
+const usePrevious = value => {
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
 
-  componentDidUpdate(prevProps, prevState) {
-    const { error } = this.props;
-    if (error.hasError && !prevState.open) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({ open: true });
+const ErrorModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const prevState = usePrevious(isOpen);
+
+  const error = useSelector(state => state.error);
+
+  useEffect(() => {
+    if (error.hasError && !prevState) {
+      setIsOpen(true);
     }
-  }
+  }, [error.hasError, prevState]);
 
-  close = () => this.setState({ open: false });
+  return (
+    <Modal size="large" open={isOpen} onClose={() => setIsOpen(false)}>
+      <Modal.Header>Sorry...</Modal.Header>
+      <Modal.Content>
+        {error.message !== '' ? (
+          <p>{error.message}</p>
+        ) : (
+          <p>
+            エラーが発生しました。ご不便をおかけし大変申し訳ございません。
+            <br />
+            <br />
+            日々改善に努めております。しばらく時間を置いて再度試しても解決されない場合は、お手数ですがお問い合わせください。
+          </p>
+        )}
+      </Modal.Content>
+      <Modal.Actions>
+        <Button onClick={() => setIsOpen(false)}>閉じる</Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
 
-  render() {
-    const { open } = this.state;
-    const { error } = this.props;
-
-    return (
-      <Modal size="large" open={open} onClose={this.close}>
-        <Modal.Header>Sorry...</Modal.Header>
-        <Modal.Content>
-          {error.message !== '' ? (
-            <p>{error.message}</p>
-          ) : (
-            <p>
-              エラーが発生しました。ご不便をおかけし大変申し訳ございません。
-              <br />
-              <br />
-              日々改善に努めております。しばらく時間を置いて再度試しても解決されない場合は、お手数ですがお問い合わせください。
-            </p>
-          )}
-        </Modal.Content>
-        <Modal.Actions>
-          <Button onClick={this.close}>閉じる</Button>
-        </Modal.Actions>
-      </Modal>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  error: state.error,
-});
-
-export default connect(mapStateToProps)(ErrorModal);
+export default ErrorModal;
