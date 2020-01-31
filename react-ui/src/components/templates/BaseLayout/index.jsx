d5fa45abe2066696ea1e2f0ca8adb285b6547fc8
Masaya Kudo <m-kudo@monooq.com>
2020-01-31 16:12:37 +0900

    footerをLV3に

--- react-ui/src/components/templates/BaseLayout/index.jsx
+++ react-ui/src/components/templates/BaseLayout/index.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from 'components/LV3/Header';
-import Footer from 'components/LV2/Footer';
+import Footer from 'components/LV3/Footer';
 
 const BaseLayout = React.memo(({ children }) => (
   <Fragment>
