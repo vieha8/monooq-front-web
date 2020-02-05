c373a02ac3cb284ac5abfd6383a13ff681c26518
Masaya Kudo <m-kudo@monooq.com>
2020-02-05 15:57:24 +0900

    aboutページのリンクをLV3コンポーネント側に

--- react-ui/src/components/pages/AboutPage/index.jsx
+++ react-ui/src/components/pages/AboutPage/index.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import Path from 'config/path';
 import About from 'components/LV3/About';
 
-const AboutPage = React.memo(({ history }) => (
-  <About
-    // TODO リンク先はcontainerで設定する必要ないのでcomponent側に移したい
-    onClickHowToUse={() => history.push(Path.howtouse())}
-    onClickInsurance={() => history.push(Path.insurance())}
-    onClickRule={() => history.push(Path.rule())}
-  />
-));
+const AboutPage = React.memo(() => <About />);
 
 export default AboutPage;
