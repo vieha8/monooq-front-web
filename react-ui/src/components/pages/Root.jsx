6b4af53ae265d775a9e483cc76ab2ecbef137edf
Masaya Kudo <m-kudo@monooq.com>
2020-02-10 17:44:22 +0900

    LP→新規登録遷移不具合一次対応

--- react-ui/src/components/pages/Root.jsx
+++ react-ui/src/components/pages/Root.jsx
@@ -41,10 +41,9 @@ class Root extends React.Component {
     if (isAvailableLocalStorage() && query.invite_code) {
       localStorage.setItem('invite_code', query.invite_code);
     }
-    const isLp = history.location.pathname.includes('/lp');
-    if (!isLp) {
-      dispatch(initActions.init());
-    }
+
+    dispatch(initActions.init());
+
     ReactGA.initialize('UA-84238514-1');
     Sentry.init({
       dsn: 'https://d3223c25da3e4dcda892c9ac1cf7b0be@sentry.io/1287932',
