51e97d780afa506d9644bb20d91f8b6639d2bb69
nayucolony <nayucolony@gmail.com>
2020-03-21 20:24:44 +0900

    minorfix

--- react-ui/src/helpers/amplitude.js
+++ react-ui/src/helpers/amplitude.js
@@ -2,7 +2,7 @@ import amplitude from 'amplitude-js/amplitude';
 
 export const initAmplitude = () => {
   const key =
-    process.env.REACT_APP_ENV === 'production'
+    process.env.REACT_APP_ENV && process.env.REACT_APP_ENV === 'production'
       ? 'f476a02df26492b1e96c72caea78fafe'
       : 'ddda257c0f722b4899f4e6b7a594cd18';
 
