6e21f3d539d05d17c6a42675fbb11be4d55e8f28
nayucolony <nayucolony@gmail.com>
2020-03-25 00:33:57 +0900

    refactor

---
+++ react-ui/src/helpers/validations/phoneNumber.js
@@ -0,0 +1,23 @@
+import { ErrorMessages } from 'variables';
+
+const isPhoneNumberValid = phoneNumber =>
+  !phoneNumber || phoneNumber.replace(/\s/g, '').length === 0;
+
+const ValidatePhoneNumber = {
+  NoHyphenVer: /^0\d{9,10}$/, // 先頭「0」+「半角数字9〜10桁」
+  HyphenVer: /^0\d{2,3}-\d{2,4}-\d{4}$/, // 先頭「0」＋「半角数字2〜3桁」＋「-」＋「半角数字1〜4桁」＋「-」＋「半角数字4桁」
+};
+
+const isPhoneNumberWithoutHyphen = phoneNumber =>
+  phoneNumber.match(ValidatePhoneNumber.NoHyphenVer) ||
+  phoneNumber.match(ValidatePhoneNumber.HyphenVer);
+
+const setErrorPhoneNumber = (v, errorsArray) => {
+  if (isPhoneNumberValid(v)) {
+    errorsArray.push(ErrorMessages.PleaseInput);
+  } else if (!isPhoneNumberWithoutHyphen(v)) {
+    errorsArray.push(ErrorMessages.InvalidPhoneNumber);
+  }
+};
+
+export { isPhoneNumberWithoutHyphen, isPhoneNumberValid, setErrorPhoneNumber };
