6f3593a3a499f096fd537220455650f607006b4c
nishisuke <ryoskns+git@gmail.com>
2020-04-09 15:28:36 +0900

    fi

--- react-ui/src/helpers/validations/tatami.js
+++ react-ui/src/helpers/validations/tatami.js
@@ -13,24 +13,20 @@ export const isValidTatami = val => {
       result: false,
       reason: ErrorMessages.PriceFloat('畳数'),
     };
-  }
-
-  if (val < Validate.Tatami.Min) {
+  } else if (val < Validate.Tatami.Min) {
     return {
       result: false,
       reason: ErrorMessages.TatamiMin(Validate.Tatami.Min),
     };
-  }
-
-  if (val > Validate.Tatami.Max) {
+  } else if (val > Validate.Tatami.Max) {
     return {
       result: false,
       reason: ErrorMessages.TatamiMax(Validate.Tatami.Max),
     };
+  } else {
+    return {
+      result: true,
+      reason: null,
+    };
   }
-
-  return {
-    result: true,
-    reason: null,
-  };
 };
