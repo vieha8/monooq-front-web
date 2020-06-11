0d425b083a367b8a0927998a22a8d36abf9fa7eb
nishisuke <ryoskns+git@gmail.com>
2020-06-11 12:36:10 +0900

    fix queue

--- react-ui/src/helpers/queue.js
+++ react-ui/src/helpers/queue.js
@@ -3,6 +3,17 @@ export class Queue {
     this.array = init.slice(0, 20);
     this.limit = 20;
   }
+
+  uniqEnqueue(item) {
+    if (this.array.includes(item)) {
+      const a = this.array.filter(_item => item != _item);
+      this.array = [...a, item];
+      return;
+    }
+
+    this.enqueue(item);
+  }
+
   enqueue(item) {
     if (this.array.length === this.limit) {
       this.array.shift();
