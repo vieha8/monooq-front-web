0553f2d7c4711146750e778cbd43f2b335245c1e
ishizuki.hiroaki <ishizuki.hiroaki@gmail.com>
2020-07-30 18:08:30 +0900

    微修正

--- react-ui/src/components/LV3/Payment/index.jsx
+++ react-ui/src/components/LV3/Payment/index.jsx
@@ -191,7 +191,8 @@ const PaymentInputForm = ({
         requestId,
         info: {
           paymentType,
-          paymentPrice: paymentType === 0 ? paymentData.priceMonthly : paymentData.price,
+          paymentPrice:
+            paymentType === 0 ? paymentData.pricePlusFeeMonthly : paymentData.pricePlusFee,
           startDate: paymentData.beginAt,
           endDate: paymentData.endAt,
           isUndecided: paymentData.isUndecided,
