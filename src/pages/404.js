fed2b1fc3851e3d1b3004d6c0b6e0a4860804229
ishizuki.hiroaki <ishizuki.hiroaki@gmail.com>
2021-01-27 20:15:54 +0900

    404 最適化

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,6 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PageNotFound from 'components/LV3/PageNotFound';
 
-const PageNotFoundPage = React.memo(() => (
-  <Fragment>
-    <PageNotFound />
-  </Fragment>
-));
+const PageNotFoundPage = React.memo(() => <PageNotFound />);
 
 export default PageNotFoundPage;
