5b03715063e81a8d9c7fc9d52151fa329dc09b26
hoangnh-hblab <hoangnh@hblab.vn>
2021-02-03 21:41:10 +0700

    update

--- src/redux/sagas/auth/index.js
+++ src/redux/sagas/auth/index.js
@@ -1,4 +1,6 @@
 import { takeEvery } from 'redux-saga/effects';
+import generators from './generators';
+
 import {
   LOGIN_EMAIL,
   LOGIN_FACEBOOK,
@@ -13,46 +15,16 @@ import {
 } from '../../actions/auth';
 
 const authSagas = [
-  takeEvery(
-    CHECK_LOGIN,
-    import('./generators').then(auth => auth.default.checkLogin),
-  ),
-  takeEvery(
-    LOGIN_EMAIL,
-    import('./generators').then(auth => auth.default.loginEmail),
-  ),
-  takeEvery(
-    LOGIN_FACEBOOK,
-    import('./generators').then(auth => auth.default.loginFacebook),
-  ),
-  takeEvery(
-    LOGOUT,
-    import('./generators').then(auth => auth.default.logout),
-  ),
-  takeEvery(
-    SIGNUP_EMAIL,
-    import('./generators').then(auth => auth.default.signUpEmail),
-  ),
-  takeEvery(
-    SIGNUP_FACEBOOK,
-    import('./generators').then(auth => auth.default.signUpFacebook),
-  ),
-  takeEvery(
-    CHECK_REDIRECT,
-    import('./generators').then(auth => auth.default.checkRedirect),
-  ),
-  takeEvery(
-    PASSWORD_RESET,
-    import('./generators').then(auth => auth.default.passwordReset),
-  ),
-  takeEvery(
-    UNSUBSCRIBE,
-    import('./generators').then(auth => auth.default.unsubscribe),
-  ),
-  takeEvery(
-    FETCH_HAS_REQUESTED,
-    import('./generators').then(auth => auth.default.fetchHasRequested),
-  ),
+  takeEvery(CHECK_LOGIN, generators.checkLogin),
+  takeEvery(LOGIN_EMAIL, generators.loginEmail),
+  takeEvery(LOGIN_FACEBOOK, generators.loginFacebook),
+  takeEvery(LOGOUT, generators.logout),
+  takeEvery(SIGNUP_EMAIL, generators.signUpEmail),
+  takeEvery(SIGNUP_FACEBOOK, generators.signUpFacebook),
+  takeEvery(CHECK_REDIRECT, generators.checkRedirect),
+  takeEvery(PASSWORD_RESET, generators.passwordReset),
+  takeEvery(UNSUBSCRIBE, generators.unsubscribe),
+  takeEvery(FETCH_HAS_REQUESTED, generators.fetchHasRequested),
 ];
 
 export default authSagas;
