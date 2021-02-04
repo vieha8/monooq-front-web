a0e6b5678700accba9775ff03a06d28d713566b2
Nguyen Van Hiep <hiepnv@hblab.vn>
2021-02-04 16:29:45 +0700

    Move SignUp page source to NextJS

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,3 +1,40 @@
-export default function SignUp() {
-  return <div>This is SignUp page</div>;
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Path from 'config/path';
+import { authActions } from 'redux/modules/auth';
+import AccountTemplate from 'components/templates/AccountTemplate';
+import LoadingPage from 'components/LV3/LoadingPage';
+import RegisterEmail from 'components/LV3/RegisterEmail';
+
+class SignUpPage extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(authActions.checkRedirect());
+  }
+
+  render() {
+    const { isInitialized, isChecking, user, router } = this.props;
+
+    if (!isInitialized || isChecking) {
+      return <LoadingPage />;
+    }
+
+    if (user.id) {
+      router.push(Path.top());
+      return null;
+    }
+
+    return <AccountTemplate title="新規登録" form={<RegisterEmail {...this.props} />} />;
+  }
 }
+
+const mapStateToProps = state => ({
+  user: state.auth.user,
+  isRegistering: state.auth.isRegistering, // TODO isRegisteringとisCheckingの役割整理
+  isChecking: state.auth.isChecking,
+  isLoading: state.user.isLoading,
+  errorMessage: state.auth.errorMessage,
+  isInitialized: state.init.isInitialized,
+});
+
+export default connect(mapStateToProps)(SignUpPage);
--- src/pages/signup/profile.js
+++ src/pages/signup/profile.js
@@ -1,3 +1,26 @@
-export default function SignUpProfile() {
-  return <div>This is SignUpProfile page</div>;
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import AccountTemplate from 'components/templates/AccountTemplate';
+import LoadingPage from 'components/LV3/LoadingPage';
+import RegisterProfile from 'components/LV3/RegisterProfile';
+
+class SignUpProfilePage extends Component {
+  render() {
+    const { user } = this.props;
+    if (!user.id) {
+      return <LoadingPage />;
+    }
+
+    return <AccountTemplate title="新規登録" form={<RegisterProfile {...this.props} />} />;
+  }
 }
+
+const mapStateToProps = state => ({
+  user: state.auth.user,
+  redirectPath: state.ui.redirectPath,
+  isRegistering: state.auth.isRegistering,
+  isLoading: state.user.isLoading,
+  isSignupFailed: state.auth.isSignupFailed,
+});
+
+export default connect(mapStateToProps)(SignUpProfilePage);
