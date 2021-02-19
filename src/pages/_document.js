d43f4a51a870f5c85797a8e4ca8bedaa5ef0d3da
vieha <viet.hq@agiletech.vn>
2021-02-19 15:21:20 +0700

    Test.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,47 +11,50 @@ export default class AppDocument extends Document {
           <link
             rel="preload"
             as="style"
-            href="https://d2j6gkx8orv8i2.cloudfront.net/css/fontawesome-all.min.css"
+            href="/css/fontawesome-all.min.css"
           />
           <link
             rel="stylesheet"
             type="text/css"
-            href="https://d2j6gkx8orv8i2.cloudfront.net/css/fontawesome-all.min.css"
+            href="/css/fontawesome-all.min.css"
           />
           <link
             rel="preload"
             as="font"
-            href="https://d2j6gkx8orv8i2.cloudfront.net/fonts/fa-light-300.woff2"
+            href="/fonts/fa-light-300.woff2"
             crossOrigin="anonymous"
           />
           <link
             rel="preload"
             as="font"
-            href="https://d2j6gkx8orv8i2.cloudfront.net/fonts/fa-solid-900.woff2"
+            href="/fonts/fa-solid-900.woff2"
             crossOrigin="anonymous"
           />
           <link
             rel="preload"
             as="font"
-            href="https://d2j6gkx8orv8i2.cloudfront.net/fonts/fa-regular-400.woff2"
+            href="/fonts/fa-regular-400.woff2"
             crossOrigin="anonymous"
           />
           <script
             defer
-            src="https://use.fontawesome.com/releases/v5.8.1/js/regular.js"
+            src="/js/regular.js"
             integrity="sha384-Uc9toywOA44owltk1MWl0lQZ+L0mBzJkLQcdif6+JtG9izvok9DLJtCZX57Uq3k2"
             crossOrigin="anonymous"
           />
           <script
             defer
-            src="https://use.fontawesome.com/releases/v5.8.1/js/fontawesome.js"
+            src="/js/fontawesome.js"
             integrity="sha384-EMmnH+Njn8umuoSMZ3Ae3bC9hDknHKOWL2e9WJD/cN6XLeAN7tr5ZQ0Hx5HDHtkS"
             crossOrigin="anonymous"
           />
         </Head>
         <body>
           <Main />
-          <NextScript />
+          <NextScript 
+          nonce
+          render={null}
+          />
         </body>
       </Html>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,126 +1,129 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Path from 'config/path';
-import { makeConditionTitle } from 'helpers/search';
-import { isAvailableLocalStorage } from 'helpers/storage';
-import { sectionActions } from 'redux/modules/section';
-import { spaceActions } from 'redux/modules/space';
-import LoadingPage from 'components/LV3/LoadingPage';
-import Top from 'components/LV3/Top';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import Path from 'config/path';
+// import { makeConditionTitle } from 'helpers/search';
+// import { isAvailableLocalStorage } from 'helpers/storage';
+// import { sectionActions } from 'redux/modules/section';
+// import { spaceActions } from 'redux/modules/space';
+// import LoadingPage from 'components/LV3/LoadingPage';
+// import Top from 'components/LV3/Top';
 
-// import RecommendedSpace from './StaticRecommendedSpace';
+// // import RecommendedSpace from './StaticRecommendedSpace';
 
-// TODO: アクセス増が想定される場合、おすすめ一覧が固定のものになるよう事前に修正しておく。
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      limit: 8,
-      offset: 0,
-    };
+// // TODO: アクセス増が想定される場合、おすすめ一覧が固定のものになるよう事前に修正しておく。
+// class Home extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       limit: 8,
+//       offset: 0,
+//     };
 
-    const { dispatch } = this.props;
+//     const { dispatch } = this.props;
 
-    dispatch(sectionActions.getRegion());
-    // dispatch(sectionActions.fetchSections());
+//     dispatch(sectionActions.getRegion());
+//     // dispatch(sectionActions.fetchSections());
 
-    // 非ログイン状態ならAction内で検索をやめてくれるのでここで分岐はしない
-    dispatch(spaceActions.doSearchMyArea());
-  }
+//     // 非ログイン状態ならAction内で検索をやめてくれるのでここで分岐はしない
+//     dispatch(spaceActions.doSearchMyArea());
+//   }
 
-  componentDidMount() {
-    const { referrer } = document;
-    if (isAvailableLocalStorage()) {
-      if (!localStorage.getItem('referrer')) {
-        localStorage.setItem('referrer', referrer);
-      }
-    }
+//   componentDidMount() {
+//     const { referrer } = document;
+//     if (isAvailableLocalStorage()) {
+//       if (!localStorage.getItem('referrer')) {
+//         localStorage.setItem('referrer', referrer);
+//       }
+//     }
 
-    const { dispatch } = this.props;
-    const { limit, offset } = this.state;
-    const params = {
-      limit,
-      offset,
-      refresh: true,
-    };
-    dispatch(spaceActions.getSpaceAccessLog(params));
-    const newOffset = offset + limit;
-    this.setState({ offset: newOffset });
-  }
+//     const { dispatch } = this.props;
+//     const { limit, offset } = this.state;
+//     const params = {
+//       limit,
+//       offset,
+//       refresh: true,
+//     };
+//     dispatch(spaceActions.getSpaceAccessLog(params));
+//     const newOffset = offset + limit;
+//     this.setState({ offset: newOffset });
+//   }
 
-  onClickSpace = spaceId => {
-    const { history } = this.props;
-    history.push(Path.space(spaceId));
-  };
+//   onClickSpace = spaceId => {
+//     const { history } = this.props;
+//     history.push(Path.space(spaceId));
+//   };
 
-  render() {
-    const {
-      // sections,
-      regionId,
-      isChecking,
-      user,
-      spaces,
-      spacesHistory,
-      conditions,
-      maxCount,
-      isLoading,
-      modalPrefName,
-    } = this.props;
+//   render() {
+//     const {
+//       // sections,
+//       regionId,
+//       isChecking,
+//       user,
+//       spaces,
+//       spacesHistory,
+//       conditions,
+//       maxCount,
+//       isLoading,
+//       modalPrefName,
+//     } = this.props;
 
-    if (isChecking) {
-      return <LoadingPage />;
-    }
+//     if (isChecking) {
+//       return <LoadingPage />;
+//     }
 
-    const conditionTitle = conditions ? makeConditionTitle(conditions) : '';
+//     const conditionTitle = conditions ? makeConditionTitle(conditions) : '';
 
-    // const sections = RecommendedSpace;
+//     // const sections = RecommendedSpace;
 
-    let isViewModalTop = false;
-    let requestParams;
-    if (user && user.isHost !== undefined && !user.isHost) {
-      if (isAvailableLocalStorage()) {
-        if (
-          localStorage.getItem('isRequestedTop') &&
-          localStorage.getItem('isRequestedTop') === 'true'
-        ) {
-          // 希望条件送付済
-        } else if (localStorage.getItem('request_params')) {
-          requestParams = localStorage.getItem('request_params');
-        } else {
-          isViewModalTop = true;
-        }
-      }
-    }
+//     let isViewModalTop = false;
+//     let requestParams;
+//     if (user && user.isHost !== undefined && !user.isHost) {
+//       if (isAvailableLocalStorage()) {
+//         if (
+//           localStorage.getItem('isRequestedTop') &&
+//           localStorage.getItem('isRequestedTop') === 'true'
+//         ) {
+//           // 希望条件送付済
+//         } else if (localStorage.getItem('request_params')) {
+//           requestParams = localStorage.getItem('request_params');
+//         } else {
+//           isViewModalTop = true;
+//         }
+//       }
+//     }
 
-    return (
-      <Top
-        // sections={sections}
-        regionId={regionId}
-        spaces={spaces}
-        spacesHistory={spacesHistory}
-        onClickSpace
-        user={user}
-        maxCount={maxCount}
-        conditionTitle={conditionTitle}
-        isViewModalTop={isViewModalTop}
-        requestParams={requestParams}
-        isLoading={isLoading}
-        modalPrefName={modalPrefName}
-      />
-    );
-  }
-}
+//     return (
+//       <Top
+//         // sections={sections}
+//         regionId={regionId}
+//         spaces={spaces}
+//         spacesHistory={spacesHistory}
+//         onClickSpace
+//         user={user}
+//         maxCount={maxCount}
+//         conditionTitle={conditionTitle}
+//         isViewModalTop={isViewModalTop}
+//         requestParams={requestParams}
+//         isLoading={isLoading}
+//         modalPrefName={modalPrefName}
+//       />
+//     );
+//   }
+// }
 
-const mapStateToProps = state => ({
-  maxCount: state.space.search.maxCount,
-  conditions: state.space.search.conditions,
-  spaces: state.space.search.results,
-  spacesHistory: state.space.spaces,
-  // sections: state.section.sections,
-  regionId: state.section.regionId,
-  user: state.auth.user,
-  isLoading: state.request.isLoading,
-  modalPrefName: state.request.prefName,
-});
+// const mapStateToProps = state => ({
+//   maxCount: state.space.search.maxCount,
+//   conditions: state.space.search.conditions,
+//   spaces: state.space.search.results,
+//   spacesHistory: state.space.spaces,
+//   // sections: state.section.sections,
+//   regionId: state.section.regionId,
+//   user: state.auth.user,
+//   isLoading: state.request.isLoading,
+//   modalPrefName: state.request.prefName,
+// });
 
-export default connect(mapStateToProps)(Home);
+// export default connect(mapStateToProps)(Home);
+
+const PageIndex = () => <div> INDEX </div>
+export default  PageIndex
