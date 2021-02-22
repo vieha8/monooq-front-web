5b4958f11d46444b3a8ae0b1df8defca84b1373c
vieha <viet.hq@agiletech.vn>
2021-02-22 15:07:38 +0700

    Add next fonts.

--- next.config.js
+++ next.config.js
@@ -1,58 +1,61 @@
+const withFonts = require('next-fonts');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
 const withImages = require('next-images');
 
-module.exports = withImages(
-  withBundleAnalyzer({
-    async rewrites() {
-      return [
-        {
-          source: '/search/:path*',
-          destination: '/spaces',
-        },
-        {
-          source: '/spaces/pref:prefecture',
-          destination: '/spaces?pref=:prefecture',
-        },
-        {
-          source: '/spaces/pref:prefecture/city:cityCode',
-          destination: '/spaces?pref=:prefecture&city=:cityCode',
-        },
-        {
-          source: '/spaces/pref:prefecture/city:cityCode/town:townCode',
-          destination: '/spaces?pref=:prefecture&city=:cityCode&town=:townCode',
-        },
-        {
-          source: '/lp1_2/guest',
-          destination: '/lp1/guest',
-        },
-        {
-          source: '/lp1_3/guest',
-          destination: '/lp1/guest',
-        },
-        {
-          source: '/lp2/guest',
-          destination: '/lp1/guest',
-        },
-        {
-          source: '/lp2_2/guest',
-          destination: '/lp1/guest',
-        },
-        {
-          source: '/lp2_3/guest',
-          destination: '/lp1/guest',
-        },
-        {
-          source: '/lp3/guest',
-          destination: '/lp1/guest',
-        },
-        {
-          source: '/lp3_3/guest',
-          destination: '/lp1/guest',
-        },
-      ];
-    },
-  }),
-);
+module.exports = withFonts(
+  withImages(
+    withBundleAnalyzer({
+      async rewrites() {
+        return [
+          {
+            source: '/search/:path*',
+            destination: '/spaces',
+          },
+          {
+            source: '/spaces/pref:prefecture',
+            destination: '/spaces?pref=:prefecture',
+          },
+          {
+            source: '/spaces/pref:prefecture/city:cityCode',
+            destination: '/spaces?pref=:prefecture&city=:cityCode',
+          },
+          {
+            source: '/spaces/pref:prefecture/city:cityCode/town:townCode',
+            destination: '/spaces?pref=:prefecture&city=:cityCode&town=:townCode',
+          },
+          {
+            source: '/lp1_2/guest',
+            destination: '/lp1/guest',
+          },
+          {
+            source: '/lp1_3/guest',
+            destination: '/lp1/guest',
+          },
+          {
+            source: '/lp2/guest',
+            destination: '/lp1/guest',
+          },
+          {
+            source: '/lp2_2/guest',
+            destination: '/lp1/guest',
+          },
+          {
+            source: '/lp2_3/guest',
+            destination: '/lp1/guest',
+          },
+          {
+            source: '/lp3/guest',
+            destination: '/lp1/guest',
+          },
+          {
+            source: '/lp3_3/guest',
+            destination: '/lp1/guest',
+          },
+        ];
+      },
+    }),
+  )
+)
