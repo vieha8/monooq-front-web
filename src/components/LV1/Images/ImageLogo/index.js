09c395af3e14f3b5e49d3a6375a85c783aab6594
vieha <viet.hq@agiletech.vn>
2021-02-22 11:00:14 +0700

    Add next/image.

--- src/components/LV1/Images/ImageLogo/index.js
+++ src/components/LV1/Images/ImageLogo/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Image from 'next/image'
 
 const logoImage = 'https://monooq.imgix.net/img%2Fservice%2Flogo.svg?auto=compress';
 const logoWhiteImage = 'https://monooq.imgix.net/img%2Fservice%2Flogo-white.svg?auto=compress';
@@ -16,12 +17,12 @@ const LogoFill = styled.img`
 `;
 
 export default {
-  Base: ({ width }) => <Logo src={logoImage} alt="logo" width={width || 280} />,
-  BaseWhite: ({ width }) => <Logo src={logoWhiteImage} alt="logo" width={width || 280} />,
-  Header: ({ width }) => <Logo src={logoImage} alt="logo" width={width || 80} />,
-  HeaderFill: () => <LogoFill src={logoImage} alt="logo" />,
-  Footer: ({ width }) => <Logo src={logoImage} alt="logo" width={width || 120} />,
-  HeaderWhite: ({ width }) => <Logo src={logoWhiteImage} alt="logo" width={width || 80} />,
-  HeaderWhiteFill: () => <LogoFill src={logoWhiteImage} alt="logo" />,
-  MonoboyBlack: () => <LogoFill src={MonoboyImage} alt="monoboy" />,
+  Base: ({ width }) => <Image loader={()=> <div />} src={logoImage} alt="logo" width={width || 280} />,
+  BaseWhite: ({ width }) => <Image loader={()=> <div />} src={logoWhiteImage} alt="logo" width={width || 280} />,
+  Header: ({ width }) => <Image loader={()=> <div />} src={logoImage} alt="logo" width={width || 80} />,
+  HeaderFill: () => <Image src={logoImage} alt="logo" />,
+  Footer: ({ width }) => <Image loader={()=> <div />} src={logoImage} alt="logo" width={width || 120} />,
+  HeaderWhite: ({ width }) => <Image loader={()=> <div />} src={logoWhiteImage} alt="logo" width={width || 80} />,
+  HeaderWhiteFill: () => <Image src={logoWhiteImage} alt="logo" />,
+  MonoboyBlack: () => <Image src={MonoboyImage} alt="monoboy" />,
 };
