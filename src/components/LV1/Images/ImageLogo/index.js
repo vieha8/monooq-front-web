a3d6b2db60c823a30f353460ee2e6a26050fc413
vieha <viet.hq@agiletech.vn>
2021-02-22 11:55:38 +0700

    Add logo test.

--- src/components/LV1/Images/ImageLogo/index.js
+++ src/components/LV1/Images/ImageLogo/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import Image from 'next/image'
 
-const logoImage = '/images/img_service_logo.svg';
+const logoImage = '/images/logo-test.png';
 // const logoWhiteImage = 'https://monooq.imgix.net/img%2Fservice%2Flogo-white.svg?auto=compress';
 // const MonoboyImage = 'https://monooq.imgix.net/img%2Fservice%2Fimg-monoboy-b.png?auto=compress';
-const logoWhiteImage = 'images/img_service_logo-white.svg';
-const MonoboyImage = 'images/img_service_img-monoboy-b.png';
+const logoWhiteImage = 'images/logo-test.png';
+const MonoboyImage = 'images/logo-test.png';
 
 const Logo = styled.img`
   width: ${props => props.width}px;
